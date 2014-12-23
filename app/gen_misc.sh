2f85f3de857bfe767595cae921c9bd9844d07a14
Simon John <git@the-jedi.co.uk>
2014-12-23 12:03:46 +0000

    Replaced all xt-* calls with their xtensa-lx106-elf-* equivalents so we don't have to create symlinks (assume windows has the xtensa* files?)
    Removed an extra call to objdump from gen_misc_plus.sh that's not in gen_misc_plus.bat - seems to make the upgrade files, but the directory never gets created so it doesn't work anyway.

--- app/gen_misc.sh
+++ app/gen_misc.sh
@@ -5,13 +5,13 @@ rm ../bin/eagle.app.v6.flash.bin ../bin/eagle.app.v6.irom0text.bin ../bin/eagle.
 
 cd .output/eagle/debug/image
 
-xt-objdump -x -s eagle.app.v6.out > ../../../../../bin/eagle.app.v6.dump
-xt-objdump -S eagle.app.v6.out > ../../../../../bin/eagle.app.v6.S
+xtensa-lx106-elf-objdump -x -s eagle.app.v6.out > ../../../../../bin/eagle.app.v6.dump
+xtensa-lx106-elf-objdump -S eagle.app.v6.out > ../../../../../bin/eagle.app.v6.S
 
-xt-objcopy --only-section .text -O binary eagle.app.v6.out eagle.app.v6.text.bin
-xt-objcopy --only-section .data -O binary eagle.app.v6.out eagle.app.v6.data.bin
-xt-objcopy --only-section .rodata -O binary eagle.app.v6.out eagle.app.v6.rodata.bin
-xt-objcopy --only-section .irom0.text -O binary eagle.app.v6.out eagle.app.v6.irom0text.bin
+xtensa-lx106-elf-objcopy --only-section .text -O binary eagle.app.v6.out eagle.app.v6.text.bin
+xtensa-lx106-elf-objcopy --only-section .data -O binary eagle.app.v6.out eagle.app.v6.data.bin
+xtensa-lx106-elf-objcopy --only-section .rodata -O binary eagle.app.v6.out eagle.app.v6.rodata.bin
+xtensa-lx106-elf-objcopy --only-section .irom0.text -O binary eagle.app.v6.out eagle.app.v6.irom0text.bin
 
 ../../../../../tools/gen_appbin.py eagle.app.v6.out v6
 
