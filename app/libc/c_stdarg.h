f1d87ee7ee1dd81eee0b60a739496b1e1d70e367
funshine <guijie.hong@gmail.com>
2014-12-26 21:24:28 +0800

    replace custom stdarg.h header file with the one in toolchain

--- app/libc/c_stdarg.h
+++ app/libc/c_stdarg.h
@@ -1,6 +1,12 @@
 #ifndef __c_stdarg_h
 #define __c_stdarg_h
 
+#if defined(__GNUC__)
+
+#include <stdarg.h>
+
+#else
+
 typedef char * va_list;
 
 #define _INTSIZEOF(n)   ((sizeof(n) + sizeof(int) - 1) & ~(sizeof(int) - 1)) 
@@ -8,6 +14,8 @@ typedef char * va_list;
 #define va_start(ap,v)  (ap = (va_list)&v + _INTSIZEOF(v))
 #define va_arg(ap,t)    (*(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)))
 #define va_end(ap)      (ap = (va_list)0)
+
+#endif
 
 #endif
 
