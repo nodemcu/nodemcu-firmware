0bbaedac898157df23e5a9d98195fc11aac201de
Till Klocke <till.klocke@gmail.com>
2015-02-07 07:05:41 +0100

    Renamed lgpio_ws2812 to match lua method name and fixed formatting

--- app/modules/ws2812.c
+++ app/modules/ws2812.c
@@ -14,20 +14,25 @@
 // ----------------------------------------------------------------------------
 // -- This WS2812 code must be compiled with -O2 to get the timing right.
 // -- http://wp.josh.com/2014/05/13/ws2812-neopixels-are-not-so-finicky-once-you-get-to-know-them/
-
 // The ICACHE_FLASH_ATTR is there to trick the compiler and get the very first pulse width correct.
-static void ICACHE_FLASH_ATTR send_ws_0(uint8_t gpio)
-{
+static void ICACHE_FLASH_ATTR send_ws_0(uint8_t gpio) {
   uint8_t i;
-  i = 4; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
-  i = 9; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
+  i = 4;
+  while (i--)
+    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
+  i = 9;
+  while (i--)
+    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
 }
 
-static void ICACHE_FLASH_ATTR send_ws_1(uint8_t gpio)
-{
+static void ICACHE_FLASH_ATTR send_ws_1(uint8_t gpio) {
   uint8_t i;
-  i = 8; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
-  i = 6; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
+  i = 8;
+  while (i--)
+    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
+  i = 6;
+  while (i--)
+    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
 }
 
 // Lua: ws2812.write(pin, "string")
@@ -35,8 +40,7 @@ static void ICACHE_FLASH_ATTR send_ws_1(uint8_t gpio)
 // ws2812.write(4, string.char(0, 255, 0)) uses GPIO2 and sets the first LED red.
 // ws2812.write(3, string.char(0, 0, 255):rep(10)) uses GPIO0 and sets ten LEDs blue.
 // ws2812.write(4, string.char(255, 0, 0, 255, 255, 255)) first LED green, second LED white.
-static int ICACHE_FLASH_ATTR lgpio_ws2812(lua_State* L)
-{
+static int ICACHE_FLASH_ATTR ws2812_write(lua_State* L) {
   const uint8_t pin = luaL_checkinteger(L, 1);
   size_t length;
   const char *buffer = luaL_checklstring(L, 2, &length);
@@ -64,18 +68,15 @@ static int ICACHE_FLASH_ATTR lgpio_ws2812(lua_State* L)
 #include "lrodefs.h"
 const LUA_REG_TYPE ws2812_map[] =
 {
-		{ LSTRKEY( "write" ), LFUNCVAL( lgpio_ws2812 ) },
-		{ LNILKEY, LNILVAL}
+  { LSTRKEY( "write" ), LFUNCVAL( ws2812_write )},
+  { LNILKEY, LNILVAL}
 };
 
-LUALIB_API int luaopen_ws2812( lua_State *L )
-{
-	// TODO: Make sure that the GPIO system is initialized
-	LREGISTER( L, "ws2812", ws2812_map );
-	return 1;
+LUALIB_API int luaopen_ws2812(lua_State *L) {
+  // TODO: Make sure that the GPIO system is initialized
+  LREGISTER(L, "ws2812", ws2812_map);
+  return 1;
 }
 
 // ----------------------------------------------------------------------------
-
-
 
