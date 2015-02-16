cb9828e35776b0680d94241fe845544e2e3329af
HuangRui <vowstar@gmail.com>
2015-02-16 22:14:37 +0800

    Add newline at the file end of dummy.c

--- app/lwip/app/dummy.c
+++ app/lwip/app/dummy.c
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * FunctionName : espconn_init
+ * Description  : dummy the espconn_init
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
 void espconn_init()
 {
 	// dummy function, do nothing.
