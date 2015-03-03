8432e8c8beacd5580e19148001c0e383f92b2ac7
Markus Gritsch <m.gritsch@gmail.com>
2015-03-03 10:20:02 +0100

    Changed the ws2812 byte order from GRB to RGB
    There seems to be interest in using the more intuitive RGB byte order to
    specify the LED color. Also fixed the documentation.

--- app/modules/ws2812.c
+++ app/modules/ws2812.c
@@ -8,47 +8,56 @@
  * from user Markus Gritsch.
  * I just put this code into its own module and pushed into a forked repo,
  * to easily create a pull request. Thanks to Markus Gritsch for the code.
- *
  */
 
 // ----------------------------------------------------------------------------
-// -- This WS2812 code must be compiled with -O2 to get the timing right.
+// -- This WS2812 code must be compiled with -O2 to get the timing right.  Read this:
 // -- http://wp.josh.com/2014/05/13/ws2812-neopixels-are-not-so-finicky-once-you-get-to-know-them/
-// The ICACHE_FLASH_ATTR is there to trick the compiler and get the very first pulse width correct.
+// -- The ICACHE_FLASH_ATTR is there to trick the compiler and get the very first pulse width correct.
 static void ICACHE_FLASH_ATTR send_ws_0(uint8_t gpio) {
   uint8_t i;
-  i = 4;
-  while (i--)
-    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
-  i = 9;
-  while (i--)
-    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
+  i = 4; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
+  i = 9; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
 }
 
 static void ICACHE_FLASH_ATTR send_ws_1(uint8_t gpio) {
   uint8_t i;
-  i = 8;
-  while (i--)
-    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
-  i = 6;
-  while (i--)
-    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
+  i = 8; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << gpio);
+  i = 6; while (i--) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << gpio);
 }
 
 // Lua: ws2812.write(pin, "string")
-// Byte triples in the string are interpreted as G R B values.
-// ws2812.write(4, string.char(0, 255, 0)) uses GPIO2 and sets the first LED red.
+// Byte triples in the string are interpreted as R G B values and sent to the hardware as G R B.
+// ws2812.write(4, string.char(255, 0, 0)) uses GPIO2 and sets the first LED red.
 // ws2812.write(3, string.char(0, 0, 255):rep(10)) uses GPIO0 and sets ten LEDs blue.
-// ws2812.write(4, string.char(255, 0, 0, 255, 255, 255)) first LED green, second LED white.
-static int ICACHE_FLASH_ATTR ws2812_write(lua_State* L) {
+// ws2812.write(4, string.char(0, 255, 0, 255, 255, 255)) first LED green, second LED white.
+static int ICACHE_FLASH_ATTR ws2812_writergb(lua_State* L)
+{
   const uint8_t pin = luaL_checkinteger(L, 1);
   size_t length;
-  const char *buffer = luaL_checklstring(L, 2, &length);
+  char *buffer = (char *)luaL_checklstring(L, 2, &length); // Cast away the constness.
 
+  // Initialize the output pin:
   platform_gpio_mode(pin, PLATFORM_GPIO_OUTPUT, PLATFORM_GPIO_FLOAT);
   platform_gpio_write(pin, 0);
-  os_delay_us(10);
 
+  // Ignore incomplete Byte triples at the end of buffer:
+  length -= length % 3;
+
+  // Rearrange R G B values to G R B order needed by WS2812 LEDs:
+  size_t i;
+  for (i = 0; i < length; i += 3) {
+    const char r = buffer[i];
+    const char g = buffer[i + 1];
+    buffer[i] = g;
+    buffer[i + 1] = r;
+  }
+
+  // Do not remove these:
+  os_delay_us(1);
+  os_delay_us(1);
+
+  // Send the buffer:
   os_intr_lock();
   const char * const end = buffer + length;
   while (buffer != end) {
@@ -68,7 +77,7 @@ static int ICACHE_FLASH_ATTR ws2812_write(lua_State* L) {
 #include "lrodefs.h"
 const LUA_REG_TYPE ws2812_map[] =
 {
-  { LSTRKEY( "write" ), LFUNCVAL( ws2812_write )},
+  { LSTRKEY( "writergb" ), LFUNCVAL( ws2812_writergb )},
   { LNILKEY, LNILVAL}
 };
 
@@ -77,6 +86,3 @@ LUALIB_API int luaopen_ws2812(lua_State *L) {
   LREGISTER(L, "ws2812", ws2812_map);
   return 1;
 }
-
-// ----------------------------------------------------------------------------
-
