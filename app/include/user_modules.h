13b08cdacf8f60fc2254e128730d696382500a84
funshine <guijie.hong@gmail.com>
2015-03-06 15:55:24 +0800

    add macro to control built-in lib for lua, add libm.a

--- app/include/user_modules.h
+++ app/include/user_modules.h
@@ -1,6 +1,15 @@
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
 
+#define LUA_USE_BUILTIN_STRING		// for string.xxx()
+#define LUA_USE_BUILTIN_TABLE		// for table.xxx()
+#define LUA_USE_BUILTIN_COROUTINE	// for coroutine.xxx()
+// #define LUA_USE_BUILTIN_MATH		// for math.xxx(), partially work
+// #define LUA_USE_BUILTIN_IO 			// for io.xxx(), partially work
+
+// #define LUA_USE_BUILTIN_OS			// for os.xxx(), not work
+// #define LUA_USE_BUILTIN_DEBUG		// for debug.xxx(), not work
+
 #define LUA_USE_MODULES
 
 #ifdef LUA_USE_MODULES
