8dc8b0eeee8acfc5f1e8f786064ea01148b9573b
Jiajie Chen <jiegec@qq.com>
2015-03-08 07:53:23 +0800

    Fix esptool.sh path

--- tools/esptool.sh
+++ tools/esptool.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+BASEDIR=$(dirname $(readlink -f "$0"))
 ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
 if [ $ret -eq 0 ]; then
     #Retry python 2
@@ -7,9 +8,9 @@ if [ $ret -eq 0 ]; then
 	    echo "Cannot find python2."
 	else 
 	    #Ok
-		python2 esptool.py $@
+		python2 $BASEDIR/esptool.py $@
 	fi
 else 
     #Ok
-	python esptool.py $@
+	python $BASEDIR/esptool.py $@
 fi
