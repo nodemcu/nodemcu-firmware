cda11a6b61a6394bcd107d9238dc7f62649c772b
vowstar <vowstar@gmail.com>
2015-07-02 21:47:04 +0800

    Warning: THIS VERSION UNDER CONSTRUCTION, CAN NOT USE. The RAM is not enough because the change of libmain.a.

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -1,12 +1,12 @@
 #ifndef __USER_VERSION_H__
 #define __USER_VERSION_H__
 
-#define NODE_VERSION_MAJOR		0U
-#define NODE_VERSION_MINOR		9U
-#define NODE_VERSION_REVISION	6U
+#define NODE_VERSION_MAJOR		1U
+#define NODE_VERSION_MINOR		1U
+#define NODE_VERSION_REVISION	2U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 0.9.6"
-#define BUILD_DATE	    "build 20150627"
+#define NODE_VERSION	"NodeMCU 1.1.2"
+#define BUILD_DATE	    "build 20150702"
 
 #endif	/* __USER_VERSION_H__ */
