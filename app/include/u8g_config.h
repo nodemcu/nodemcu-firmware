1bc156e4d15b64970a7652877f65ceabb00d9e29
devsaurus <devsaurus@users.noreply.github.com>
2015-07-16 23:23:43 +0200

    move display constructors into template-based functions + uniform generaration + addition of further displays - slight unneccesary heap allocation when display drivers are included but not used

--- app/include/u8g_config.h
+++ app/include/u8g_config.h
@@ -2,20 +2,38 @@
 #define __U8G_CONFIG_H__
 
 
+// ***************************************************************************
 // Configure U8glib fonts
-// add a U8G_FONT_TABLE_ENTRY for each font you want to compile into the image
+//
+// Add a U8G_FONT_TABLE_ENTRY for each font you want to compile into the image
 #define U8G_FONT_TABLE_ENTRY(font)
 #define U8G_FONT_TABLE \
     U8G_FONT_TABLE_ENTRY(font_6x10)  \
     U8G_FONT_TABLE_ENTRY(font_chikita)
 #undef U8G_FONT_TABLE_ENTRY
+//
+// ***************************************************************************
 
 
+// ***************************************************************************
 // Enable display drivers
-#define U8G_SSD1306_128x64_I2C
-//#define U8G_SSD1306_128x64_SPI
-// untested
-#undef U8G_PCD8544_84x48
+//
+// Uncomment the U8G_DISPLAY_TABLE_ENTRY for the device(s) you want to
+// compile into the firmware.
+// Stick to the assignments to *_I2C and *_SPI tables.
+//
+// I2C based displays go into here:
+#define U8G_DISPLAY_TABLE_ENTRY(device, display)
+#define U8G_DISPLAY_TABLE_I2C \
+    U8G_DISPLAY_TABLE_ENTRY(ssd1306_128x64_i2c, ssd1306_128x64)
+
+// SPI based displays go into here:
+#define U8G_DISPLAY_TABLE_SPI \
+    U8G_DISPLAY_TABLE_ENTRY(ssd1306_128x64_hw_spi, ssd1306_128x64) \
+//    U8G_DISPLAY_TABLE_ENTRY(pcd8544_84x48_hw_spi, pcd8544_84x48)
+#undef U8G_DISPLAY_TABLE_ENTRY
+//
+// ***************************************************************************
 
 
 #endif	/* __U8G_CONFIG_H__ */
