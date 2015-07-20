719abca418171fe3771898feb8a56b25fdf4c95a
Johny Mattsson <jmattsson@dius.com.au>
2015-07-20 18:02:16 +1000

    Stop cjson from killing the node on out-of-mem.

---
+++ app/cjson/cjson_mem.h
@@ -0,0 +1,11 @@
+#ifndef _CJSON_MEM_H_
+#define _CJSON_MEM_H_
+
+#include "../lua/lua.h"
+
+void cjson_mem_setlua (lua_State *L);
+
+void *cjson_mem_malloc (uint32_t sz);
+void *cjson_mem_realloc (void *p, uint32_t sz);
+
+#endif
