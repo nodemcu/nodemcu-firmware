fd9113124c004eea88966b4806de4cd10dfa1a42
Johny Mattsson <johny.mattsson+github.gmail.com>
2015-12-16 18:59:55 +1100

    Corrected the 4-page alignment for start of fs.

--- app/spiffs/spiffs.c
+++ app/spiffs/spiffs.c
@@ -51,7 +51,7 @@ void myspiffs_mount() {
 #else
   cfg.phys_addr = ( u32_t )platform_flash_get_first_free_block_address( NULL ); 
 #endif
-  cfg.phys_addr += 0x3000;
+  cfg.phys_addr += 0x3FFF;
   cfg.phys_addr &= 0xFFFFC000;  // align to 4 sector.
   cfg.phys_size = INTERNAL_FLASH_SIZE - ( ( u32_t )cfg.phys_addr );
   cfg.phys_erase_block = INTERNAL_FLASH_SECTOR_SIZE; // according to datasheet
@@ -94,7 +94,7 @@ int myspiffs_format( void )
 #else
   sect_first = ( u32_t )platform_flash_get_first_free_block_address( NULL ); 
 #endif
-  sect_first += 0x3000;
+  sect_first += 0x3FFF;
   sect_first &= 0xFFFFC000;  // align to 4 sector.
   sect_first = platform_flash_get_sector_of_address(sect_first);
   sect_last = INTERNAL_FLASH_SIZE - SYS_PARAM_SEC_NUM;
