48f3cbffc539986b8cd972301a5d5842888b1270
Huang Rui <vowstar@gmail.com>
2015-12-29 16:53:28 +0800

    Add HTTP GET method

--- app/modules/http.c
+++ app/modules/http.c
@@ -180,6 +180,36 @@ static int http_lapi_delete( lua_State *L )
   return 0;
 }
 
+// Lua: http.get( url, header, function(status, reponse) end )
+static int http_lapi_get( lua_State *L )
+{
+  int length;
+  http_client_L        = L;
+  const char * url     = luaL_checklstring(L, 1, &length);
+  const char * headers = NULL;
+
+  // Check parameter
+  if ((url == NULL))
+  {
+    return luaL_error( L, "wrong arg type" );
+  }
+
+  if (lua_isstring(L, 2))
+  {
+    headers = luaL_checklstring(L, 2, &length);
+  }
+
+  if (lua_type(L, 3) == LUA_TFUNCTION || lua_type(L, 3) == LUA_TLIGHTFUNCTION) {
+  lua_pushvalue(L, 3);  // copy argument (func) to the top of stack
+    if (http_callback_registry != LUA_NOREF)
+      luaL_unref(L, LUA_REGISTRYINDEX, http_callback_registry);
+    http_callback_registry = luaL_ref(L, LUA_REGISTRYINDEX);
+  }
+
+  http_get(url, headers, http_callback);
+  return 0;
+}
+
 // Module function map
 static const LUA_REG_TYPE http_map[] = {
   { LSTRKEY( "request" ),         LFUNCVAL( http_lapi_request ) },
