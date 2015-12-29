59c630b04924104b916dce86dd78aaf93f21770b
Huang Rui <vowstar@gmail.com>
2015-12-29 15:13:26 +0800

    Check parameter before do HTTP request

--- app/modules/http.c
+++ app/modules/http.c
@@ -22,7 +22,14 @@ static void http_callback( char * response, int http_status, char * full_respons
     lua_rawgeti(http_client_L, LUA_REGISTRYINDEX, http_callback_registry);
 
     lua_pushnumber(http_client_L, http_status);
-    lua_pushstring(http_client_L, response);
+    if ( http_status != HTTP_STATUS_GENERIC_ERROR )
+    {
+      lua_pushstring(http_client_L, response);
+    }
+    else
+    {
+      lua_pushnil(http_client_L);
+    }
     lua_call(http_client_L, 2, 0); // With 2 arguments and 0 result
 
     luaL_unref(http_client_L, LUA_REGISTRYINDEX, http_callback_registry);
@@ -39,10 +46,14 @@ static int http_lapi_request( lua_State *L )
   const char * method  = luaL_checklstring(L, 2, &length);
   const char * headers = luaL_checklstring(L, 3, &length);
   const char * body    = luaL_checklstring(L, 4, &length);
+  if ((url == NULL) || (method == NULL))
+  {
+    return luaL_error( L, "wrong arg type" );
+  }
 
-  if (lua_type(L, 5) == LUA_TFUNCTION || lua_type(L, 5) == LUA_TLIGHTFUNCTION){
+  if (lua_type(L, 5) == LUA_TFUNCTION || lua_type(L, 5) == LUA_TLIGHTFUNCTION) {
     lua_pushvalue(L, 5);  // copy argument (func) to the top of stack
-    if(http_callback_registry != LUA_NOREF)
+    if (http_callback_registry != LUA_NOREF)
       luaL_unref(L, LUA_REGISTRYINDEX, http_callback_registry);
     http_callback_registry = luaL_ref(L, LUA_REGISTRYINDEX);
   }
