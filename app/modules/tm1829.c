6700a8f06f49cfcf71129a0e0b958dcca21fe991
sebi2k1 <dev@sebastianhaas.info>
2016-05-15 21:38:06 +0200

    Reworked bit bang algorithm to avoid glitches

--- app/modules/tm1829.c
+++ app/modules/tm1829.c
@@ -13,51 +13,57 @@ static inline uint32_t _getCycleCount(void) {
 
 // This algorithm reads the cpu clock cycles to calculate the correct
 // pulse widths. It works in both 80 and 160 MHz mode.
-// The values for t0l, t1l, ttot have been tweaked and it doesn't get faster than this.
-// The datasheet is confusing and one might think that a shorter pulse time can be achieved.
-// The period has to be at least 1.25us, even if the datasheet says:
-//   T0H: 0.35 (+- 0.15) + T0L: 0.8 (+- 0.15), which is 0.85<->1.45 us.
-//   T1H: 0.70 (+- 0.15) + T1L: 0.6 (+- 0.15), which is 1.00<->1.60 us.
-// Anything lower than 1.25us will glitch in the long run.
 static void ICACHE_RAM_ATTR tm1829_write(uint8_t pin, uint8_t *pixels, uint32_t length) {
-  uint8_t *p, *end, pixel, mask;
-  uint32_t t, t0l, t1l, ttot, c, start_time, pin_mask;
+  uint8_t *p, *end;
 
-  pin_mask = 1 << pin;
-  p =  pixels;
+  p   =  pixels;
   end =  p + length;
-  pixel = *p++;
-  mask = 0x80;
 
-  t0l  = (1000 * system_get_cpu_freq()) / 3022;  // 0.35us (spec=0.35 +- 0.15)
-  t1l  = (1000 * system_get_cpu_freq()) / 1477;  // 0.70us (spec=0.70 +- 0.15)
-  ttot = (1000 * system_get_cpu_freq()) /  800;  // 1.25us (MUST be >= 1.25)
+  const uint32_t t0l  = (1000 * system_get_cpu_freq()) / 3333;  // 0.390us (spec=0.35 +- 0.15)
+  const uint32_t t1l  = (1000 * system_get_cpu_freq()) / 1250;  // 0.800us (spec=0.70 +- 0.15)
 
-  while (true) {
-    if (pixel & mask)
-        t = t1l;
-    else
-        t = t0l;
+  const uint32_t ttot = (1000 * system_get_cpu_freq()) / 450;   // 2.2us (MUST be >= 1.25)
 
-    start_time = _getCycleCount();                        // Save the start time
-    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, pin_mask);      // Set pin low
-    while (((c = _getCycleCount()) - start_time) < t);    // Wait for low time to finish
+  const uint32_t tbreak = (1000 * system_get_cpu_freq()) / 160; // 6us
 
-    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, pin_mask);      // Set pin high
-    while (((c = _getCycleCount()) - start_time) < ttot); // Wait for the previous bit to finish
+  while (p != end) {
+    register int i;
 
-    if (!(mask >>= 1)) {                                  // Next bit/byte
-      if (p >= end)
-        break;
+    register uint8_t pixel = *p++;
 
-      pixel = *p++;
-      mask  = 0x80;
+    for (i = 7; i >= 0; i--) {
+       register uint32_t pin_mask = 1 << pin;
+
+       // Select low time
+       register uint32_t tl = ((pixel >> i) & 1) ? t1l : t0l;
+       register uint32_t t1, t2;
+
+       register uint32_t t = _getCycleCount();
+
+       t1 = t + tl;
+       t2 = t + ttot;
+
+       GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, pin_mask); // Set pin low
+       while (_getCycleCount() < t1);
+
+       GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, pin_mask); // Set pin high
+       while (_getCycleCount() < t2);
+    }
+
+    {
+      register uint32_t t = _getCycleCount() + tbreak;
+      while (_getCycleCount() < t);
     }
   }
 }
 
-// Lua: tm1829.writergb(pin, "string")
+// Lua: tm1829.write(pin, "string")
 // Byte triples in the string are interpreted as R G B values and sent to the hardware as G R B.
+// WARNING: this function scrambles the input buffer :
+//    a = string.char(255,0,128)
+//    tm1829.write(3,a)
+//    =a.byte()
+//    (0,255,128)
 
 // tm1829.write(4, string.char(255, 0, 0)) uses GPIO2 and sets the first LED red.
 // tm1829.write(3, string.char(0, 0, 255):rep(10)) uses GPIO0 and sets ten LEDs blue.
@@ -75,12 +81,17 @@ static int ICACHE_FLASH_ATTR tm1829_writergb(lua_State* L)
   // Ignore incomplete Byte triples at the end of buffer:
   length -= length % 3;
 
-  // Rearrange R G B values to G R B order needed by tm1829 LEDs:
+  // Rearrange R G B values to B R G order needed by tm1829 LEDs:
   size_t i;
   for (i = 0; i < length; i += 3) {
-    const char r = buffer[i];
-    const char g = buffer[i + 1];
-    const char b = buffer[i + 2];
+    char r = buffer[i];
+    char g = buffer[i + 1];
+    char b = buffer[i + 2];
+
+    // First 8 bit must not be 1s (constant current command,
+    // instead of PWM duty command)
+    if (b == 0xff)
+        b = 0xfe;
 
     buffer[i] = b;
     buffer[i + 1] = r;
@@ -89,16 +100,14 @@ static int ICACHE_FLASH_ATTR tm1829_writergb(lua_State* L)
 
   // Initialize the output pin and wait a bit
   platform_gpio_mode(pin, PLATFORM_GPIO_OUTPUT, PLATFORM_GPIO_FLOAT);
-  platform_gpio_write(pin, PLATFORM_GPIO_HIGH);
-
-  // Make pin high for 500us to make sure chip is ready to receive
-  // data
-  os_delay_us(500);
+  platform_gpio_write(pin, 1);
 
   // Send the buffer
   ets_intr_lock();
   tm1829_write(pin_num[pin], (uint8_t*) buffer, length);
   ets_intr_unlock();
+
+  os_delay_us(500); // reset time
 
   c_free(buffer);
 
