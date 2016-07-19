56839fb20b035f7f2bb5059d215007f092d7d04b
Arnim Läuger <devsaurus@users.noreply.github.com>
2016-07-19 22:01:26 +0200

    Add rtctime.epoch2cal() (#1386)

--- app/include/rtc/rtctime.h
+++ app/include/rtc/rtctime.h
@@ -49,6 +49,17 @@ struct rtc_timeval
 };
 #endif
 
+struct rtc_tm{
+  int tm_sec;   /* Seconds.     [0-60] (1 leap second) */
+  int tm_min;   /* Minutes.     [0-59] */
+  int tm_hour;  /* Hours.       [0-23] */
+  int tm_mday;  /* Day.         [1-31] */
+  int tm_mon;   /* Month.       [0-11] */
+  int tm_year;  /* Year - 1900. */
+  int tm_wday;  /* Day of week. [0-6] */
+  int tm_yday;  /* Days in year.[0-365]	*/
+};
+
 void TEXT_SECTION_ATTR rtctime_early_startup (void);
 void rtctime_late_startup (void);
 void rtctime_gettimeofday (struct rtc_timeval *tv);
@@ -56,5 +67,6 @@ void rtctime_settimeofday (const struct rtc_timeval *tv);
 bool rtctime_have_time (void);
 void rtctime_deep_sleep_us (uint32_t us);
 void rtctime_deep_sleep_until_aligned_us (uint32_t align_us, uint32_t min_us);
+void rtctime_gmtime (const int32 stamp, struct rtc_tm *r);
 
 #endif
