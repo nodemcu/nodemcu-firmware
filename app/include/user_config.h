78beb45ed5590ad5e363ec5c8b1c4216fc66f984
Yury Popov <github@phoenix.dj>
2016-08-21 01:07:47 +0300

    Set espconn's secure buffer size once at initialization Issue: #1457

--- app/include/user_config.h
+++ app/include/user_config.h
@@ -60,6 +60,10 @@ extern void luaL_assertfail(const char *file, int line, const char *message);
 #define NO_INTR_CODE inline
 #endif
 
+// SSL buffer size used only for espconn-layer secure connections.
+// See https://github.com/nodemcu/nodemcu-firmware/issues/1457 for conversation details.
+#define SSL_BUFFER_SIZE 5120
+
 //#define CLIENT_SSL_ENABLE
 //#define MD2_ENABLE
 #define SHA2_ENABLE
