6f54c38e535f6a3f8185eba82e530b6578e38431
Johny Mattsson <jmattsson@dius.com.au>
2016-09-21 18:21:44 +1000

    Grabbed fatfs from dev branch, and IDF-ified it.

---
+++ components/fatfs/component.mk
@@ -0,0 +1,8 @@
+COMPONENT_SRCDIRS:=. option
+# TODO: add Kconfig to select code page used?
+COMPONENT_OBJS:=diskio.o ff.o myfatfs.o option/unicode.o 
+COMPONENT_ADD_INCLUDEDIRS:=.
+
+EXTRA_CFLAGS:=-imacros fatfs_prefix_lib.h
+
+include $(IDF_PATH)/make/component_common.mk
