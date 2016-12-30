a0995ee321248a6d52f9b21aa09fcac223709571
Verne Roberts <verneroberts@gmail.com>
2016-12-31 11:31:40 +1300

    added 31250 baud rate for MIDI support

--- app/platform/platform.c
+++ app/platform/platform.c
@@ -361,6 +361,7 @@ uint32_t platform_uart_setup( unsigned id, uint32_t baud, int databits, int pari
     case BIT_RATE_4800:
     case BIT_RATE_9600:
     case BIT_RATE_19200:
+    case BIT_RATE_31250:
     case BIT_RATE_38400:
     case BIT_RATE_57600:
     case BIT_RATE_74880:
