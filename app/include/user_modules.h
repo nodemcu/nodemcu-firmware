f76f873882c8b862a20f03e61c4417c3717affd9
Wolfgang Rosner <wrosner@tirnet.de>
2017-01-16 22:01:04 +0100

    try to establish individualized clean SDK2.0.0 build

--- app/include/user_modules.h
+++ app/include/user_modules.h
@@ -5,10 +5,10 @@
 #define LUA_USE_BUILTIN_TABLE		// for table.xxx()
 #define LUA_USE_BUILTIN_COROUTINE	// for coroutine.xxx()
 #define LUA_USE_BUILTIN_MATH		// for math.xxx(), partially work
-// #define LUA_USE_BUILTIN_IO 			// for io.xxx(), partially work
+#define LUA_USE_BUILTIN_IO 			// for io.xxx(), partially work
 
 // #define LUA_USE_BUILTIN_OS			// for os.xxx(), not work
-// #define LUA_USE_BUILTIN_DEBUG
+#define LUA_USE_BUILTIN_DEBUG
 #define LUA_USE_BUILTIN_DEBUG_MINIMAL // for debug.getregistry() and debug.traceback()
 
 #ifndef LUA_CROSS_COMPILER
@@ -28,10 +28,10 @@
 //#define LUA_USE_MODULES_COAP
 //#define LUA_USE_MODULES_CRON
 //#define LUA_USE_MODULES_CRYPTO
-#define LUA_USE_MODULES_DHT
+//#define LUA_USE_MODULES_DHT
 //#define LUA_USE_MODULES_ENCODER
 //#define LUA_USE_MODULES_ENDUSER_SETUP // USE_DNS in dhcpserver.h needs to be enabled for this module to work.
-#define LUA_USE_MODULES_FILE
+//#define LUA_USE_MODULES_FILE
 //#define LUA_USE_MODULES_GDBSTUB
 #define LUA_USE_MODULES_GPIO
 //#define LUA_USE_MODULES_HMC5883L
@@ -40,24 +40,24 @@
 #define LUA_USE_MODULES_I2C
 //#define LUA_USE_MODULES_L3G4200D
 //#define LUA_USE_MODULES_MDNS
-#define LUA_USE_MODULES_MQTT
+//#define LUA_USE_MODULES_MQTT
 #define LUA_USE_MODULES_NET
 #define LUA_USE_MODULES_NODE
 #define LUA_USE_MODULES_OW
 //#define LUA_USE_MODULES_PCM
 //#define LUA_USE_MODULES_PERF
-//#define LUA_USE_MODULES_PWM
+#define LUA_USE_MODULES_PWM
 //#define LUA_USE_MODULES_RC
 //#define LUA_USE_MODULES_RFSWITCH
-//#define LUA_USE_MODULES_ROTARY
+#define LUA_USE_MODULES_ROTARY
 //#define LUA_USE_MODULES_RTCFIFO
 //#define LUA_USE_MODULES_RTCMEM
 //#define LUA_USE_MODULES_RTCTIME
 //#define LUA_USE_MODULES_SIGMA_DELTA
-//#define LUA_USE_MODULES_SNTP
+#define LUA_USE_MODULES_SNTP
 //#define LUA_USE_MODULES_SOMFY
 #define LUA_USE_MODULES_SPI
-//#define LUA_USE_MODULES_STRUCT
+#define LUA_USE_MODULES_STRUCT
 //#define LUA_USE_MODULES_SWITEC
 //#define LUA_USE_MODULES_TM1829
 #define LUA_USE_MODULES_TLS
