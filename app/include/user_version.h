061e581c75d1e72a1f8e8c03a49760ce63871963
Wolfgang Rosner <wrosner@tirnet.de>
2017-01-23 18:14:05 +0100

    revert personalized configuration

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 2.0.0-wolfgangr"
+#define NODE_VERSION	"NodeMCU 2.0.0"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "20170116"
+#define BUILD_DATE	  "unspecified"
 #endif
 
 extern char SDK_VERSION[];
