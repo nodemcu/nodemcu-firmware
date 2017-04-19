7386f342b5742e11f7ce7ef0de69426814206909
Arnim Läuger <devsaurus@users.noreply.github.com>
2017-04-20 01:40:53 +0200

    ESP32: Use fwrite() instead of printf() for printing strings from Lua. (#1919)
    * use fwrite() instead of printf() for printing strings from Lua
    
    Fixes #1914.

--- components/lua/luaconf.h
+++ components/lua/luaconf.h
@@ -331,7 +331,7 @@ extern int readline4lua(const char *prompt, char *buffer, int length);
 ** avoids including 'stdio.h' everywhere.)
 */
 #if !defined(LUA_USE_STDIO)
-#define luai_writestring(s, l)  printf(s)
+#define luai_writestring(s, l)  fwrite(s, 1, l, stdout)
 #define luai_writeline()        puts("")
 #endif // defined(LUA_USE_STDIO)
 
