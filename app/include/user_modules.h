faefc095959832c5eaf7e6a9dcaae957fe42ac6a
fetchbot <fetchbot@users.noreply.github.com>
2017-05-07 09:49:57 +0200

    add ads1115 module (#1942)
    * add ads1115 module
    
    * replace os_delay with os_timer
    
    * typo

--- app/include/user_modules.h
+++ app/include/user_modules.h
@@ -18,6 +18,7 @@
 // See https://github.com/nodemcu/nodemcu-firmware/pull/1127 for discussions.
 // New modules should be disabled by default and added in alphabetical order.
 #define LUA_USE_MODULES_ADC
+//#define LUA_USE_MODULES_ADS1115
 //#define LUA_USE_MODULES_ADXL345
 //#define LUA_USE_MODULES_AM2320
 //#define LUA_USE_MODULES_APA102
@@ -78,4 +79,3 @@
 
 #endif  /* LUA_CROSS_COMPILER */
 #endif	/* __USER_MODULES_H__ */
-
