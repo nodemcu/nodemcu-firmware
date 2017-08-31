b64a69b4da0156d2a65cefa9cce3c2633289bea2
Lars Stenberg <lars@cacharge.com>
2017-08-31 09:53:25 +0200

    Redesigned to only handle OTA app partions

--- components/modules/ota.c
+++ components/modules/ota.c
@@ -116,15 +116,6 @@ static int ota_complete( lua_State *L )
 #define OTA_PARTITION_INFO(L, partition) \
   lua_newtable(L); \
   \
-  lua_pushinteger(L, (int)partition); \
-  lua_setfield(L, -2, "handle"); \
-  \
-  lua_pushinteger(L, partition->type); \
-  lua_setfield(L, -2, "type"); \
-  \
-  lua_pushinteger(L, partition->subtype); \
-  lua_setfield(L, -2, "subtype"); \
-  \
   lua_pushinteger(L, partition->address); \
   lua_setfield(L, -2, "address"); \
   \
@@ -137,15 +128,13 @@ static int ota_complete( lua_State *L )
   lua_pushboolean(L, partition->encrypted); \
   lua_setfield(L, -2, "encrypted");
 
-static int ota_partition_find_first( lua_State *L ) {
+static int ota_partition_find( lua_State *L ) {
   int stack = 0;
 
-  int type_filter = luaL_checkint (L, ++stack);
-  int subtype_filter = luaL_checkint (L, ++stack);
   size_t label_length;
   const char *label_filter = luaL_optlstring(L, ++stack, NULL, &label_length);
 
-  const esp_partition_t * partition = esp_partition_find_first(type_filter, subtype_filter, label_filter);
+  const esp_partition_t * partition = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, label_filter);
   if(!partition)
     return 0;
 
@@ -154,20 +143,14 @@ static int ota_partition_find_first( lua_State *L ) {
   return 1;
 }
 
-static int ota_partition_find( lua_State *L ) {
-  int stack = 0;
-
-  int type_filter = luaL_checkint (L, ++stack);
-  int subtype_filter = luaL_checkint (L, ++stack);
-  size_t label_length;
-  const char *label_filter = luaL_optlstring(L, ++stack, NULL, &label_length);
-
-  esp_partition_iterator_t iterator = esp_partition_find(type_filter, subtype_filter, label_filter);
+static int ota_partition_find_all( lua_State *L ) {
+  esp_partition_iterator_t iterator = esp_partition_find(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, NULL);
   lua_newtable(L);
 
   int i=0;
   while(iterator) {
     const esp_partition_t * partition = esp_partition_get(iterator);
+
     lua_pushnumber(L, ++i);
 
     OTA_PARTITION_INFO(L, partition);
@@ -198,7 +181,14 @@ static int ota_partition_get_boot_partition( lua_State *L ) {
 }
 
 static int ota_partition_set_boot( lua_State *L ) {
-  const esp_partition_t *partition = (esp_partition_t *)luaL_checkint (L, 1);
+  int stack = 0;
+
+  const char *label_filter = luaL_checkstring(L, ++stack);
+
+  const esp_partition_t * partition = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, label_filter);
+  if(!partition) {
+    OTA_ERROR(L, "set_boot failed! partition '%s' not found", label_filter);
+  }
 
   esp_err_t err = esp_ota_set_boot_partition(partition);
   if (err != ESP_OK) {
@@ -212,44 +202,12 @@ static int ota_partition_set_boot( lua_State *L ) {
 /* Maps */
 
 static const LUA_REG_TYPE ota_partition_map[] = {
-  { LSTRKEY( "find" ),                   LFUNCVAL( ota_partition_find ) },
-  { LSTRKEY( "find_first"),              LFUNCVAL( ota_partition_find_first ) },
+  { LSTRKEY( "find_all" ),               LFUNCVAL( ota_partition_find_all ) },
+  { LSTRKEY( "find"),                    LFUNCVAL( ota_partition_find ) },
 
   { LSTRKEY( "get_running"),             LFUNCVAL( ota_partition_get_running_partition ) },
   { LSTRKEY( "get_boot"),                LFUNCVAL( ota_partition_get_boot_partition ) },
   { LSTRKEY( "set_boot"),                LFUNCVAL( ota_partition_set_boot ) },
-
-  { LSTRKEY( "TYPE_APP"),                LNUMVAL( ESP_PARTITION_TYPE_APP ) },
-  { LSTRKEY( "TYPE_DATA"),               LNUMVAL( ESP_PARTITION_TYPE_DATA  ) },
-
-  { LSTRKEY( "SUBTYPE_APP_FACTORY"),     LNUMVAL( ESP_PARTITION_SUBTYPE_APP_FACTORY ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_MIN"),     LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_MIN ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_0"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_0 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_1"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_1 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_2"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_2 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_3"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_3 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_4"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_4 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_5"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_5 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_6"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_6 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_7"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_7 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_8"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_8 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_9"),       LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_9 ) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_10"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_10) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_11"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_11) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_12"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_12) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_13"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_13) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_14"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_14) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_15"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_15) },
-  { LSTRKEY( "SUBTYPE_APP_OTA_16"),      LNUMVAL( ESP_PARTITION_SUBTYPE_APP_OTA_MAX ) },
-  { LSTRKEY( "SUBTYPE_APP_TEST"),        LNUMVAL( ESP_PARTITION_SUBTYPE_APP_TEST ) },
-  { LSTRKEY( "SUBTYPE_DATA_OTA"),        LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_OTA ) },
-  { LSTRKEY( "SUBTYPE_DATA_PHY"),        LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_PHY ) },
-  { LSTRKEY( "SUBTYPE_DATA_NVS"),        LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_NVS ) },
-  { LSTRKEY( "SUBTYPE_DATA_COREDUMP"),   LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_COREDUMP ) },
-  { LSTRKEY( "SUBTYPE_DATA_ESPHTTPD"),   LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_ESPHTTPD ) },
-  { LSTRKEY( "SUBTYPE_DATA_FAT"),        LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_FAT ) },
-  { LSTRKEY( "SUBTYPE_DATA_SPIFFS"),     LNUMVAL( ESP_PARTITION_SUBTYPE_DATA_SPIFFS ) },
-  { LSTRKEY( "SUBTYPE_ANY"),        LNUMVAL( ESP_PARTITION_SUBTYPE_ANY ) },
 
   { LNILKEY, LNILVAL }
 };
