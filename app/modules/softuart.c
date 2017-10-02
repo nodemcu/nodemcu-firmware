65c98ba693ef8aea5f97e0f113a0f0d8af4e181d
Christian Krämer <christian.kraemer@tngtech.com>
2017-10-02 21:56:52 +0200

    Use infrastructure from platform.c and make it interoperate with other interrupts

--- app/modules/softuart.c
+++ app/modules/softuart.c
@@ -12,14 +12,6 @@
 
 #define SOFTUART_MAX_RX_BUFF 256
 
-//array of pointers to instances
-
-typedef struct softuart_pin_t {
-	uint8_t gpio_id;
-	uint32_t gpio_mux_name;
-	uint8_t gpio_func;
-} softuart_pin_t;
-
 typedef struct softuart_buffer_t {
 	char receive_buffer[SOFTUART_MAX_RX_BUFF]; 
 	uint8_t length;
@@ -27,8 +19,8 @@ typedef struct softuart_buffer_t {
 
 typedef struct {
     BOOL enabled;
-	softuart_pin_t pin_rx;
-	softuart_pin_t pin_tx;
+    uint8_t pin_rx;
+    uint8_t pin_tx;
 	softuart_buffer_t buffer; //TODO volatile ??? not needed as no read anymore
     uint32_t baudrate;
 	uint16_t bit_time;
@@ -45,27 +37,6 @@ typedef struct {
 	uint8_t gpio_func;
 } softuart_reg_t;
 
-softuart_reg_t softuart_reg[] =
-{
-	{ PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0 },	//gpio0
-	{ PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1 },	//gpio1 (uart)
-	{ PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2 },	//gpio2
-	{ PERIPHS_IO_MUX_U0RXD_U, FUNC_GPIO3 },	//gpio3 (uart)
-	{ PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4 },	//gpio4
-	{ PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5 },	//gpio5
-	{ 0, 0 },
-	{ 0, 0 },
-	{ 0, 0 },
-	{ 0, 0 },
-	{ 0, 0 },
-	{ 0, 0 },
-	{ PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12 },	//gpio12
-	{ PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13 },	//gpio13
-	{ PERIPHS_IO_MUX_MTMS_U, FUNC_GPIO14 },	//gpio14
-	{ PERIPHS_IO_MUX_MTDO_U, FUNC_GPIO15 },	//gpio15
-	//@TODO TODO gpio16 is missing (?include)
-};
-
 bool softuart_on_data_cb(char *buf, size_t len);
 
 uint8_t softuart_gpio_valid(uint8_t gpio_id)
@@ -79,47 +50,29 @@ uint8_t softuart_gpio_valid(uint8_t gpio_id)
 
 void softuart_set_rx(uint8_t gpio_id)
 {
-    softuart.pin_rx.gpio_id = gpio_id;
-    softuart.pin_rx.gpio_mux_name = softuart_reg[gpio_id].gpio_mux_name;
-    softuart.pin_rx.gpio_func = softuart_reg[gpio_id].gpio_func;
+    softuart.pin_rx = gpio_id;
 }
 
 void softuart_set_tx(uint8_t gpio_id)
 {
-    softuart.pin_tx.gpio_id = gpio_id;
-    softuart.pin_tx.gpio_mux_name = softuart_reg[gpio_id].gpio_mux_name;
-    softuart.pin_tx.gpio_func = softuart_reg[gpio_id].gpio_func;
+    softuart.pin_tx = gpio_id;
 }
 
-uint8_t softuart_bitcount(uint32_t x)
-{
-	uint8_t count;
- 
-	for (count=0; x != 0; x>>=1) {
-		if ( x & 0x01) {
-			return count;
-		}
-		count++;
-	}
-	//error: no 1 found!
-	return 0xFF;
-}
-
-void softuart_intr_handler()
+//TODO ICACHE_RAM_ATTR
+static uint32_t softuart_intr_handler(uint32_t ret_gpio_status)
 {
     Softuart* s = &softuart;
-    uint8_t level, gpio_id;
-    // clear gpio status. Say ESP8266EX SDK Programming Guide in  5.1.6. GPIO interrupt handler
-
+    uint8_t level;
+    uint32_t mask = 1 << s->pin_rx;
     uint32_t gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
-    gpio_id = softuart_bitcount(gpio_status);
 
-    //if interrupt was by an attached rx pin
-    if (gpio_id != 0xFF)
-    {
-        // disable interrupt for GPIO0
-        gpio_pin_intr_state_set(GPIO_ID_PIN(s->pin_rx.gpio_id), GPIO_PIN_INTR_DISABLE);
-        level = GPIO_INPUT_GET(GPIO_ID_PIN(s->pin_rx.gpio_id));
+    // clear gpio status. Say ESP8266EX SDK Programming Guide in  5.1.6. GPIO interrupt handler
+    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status & mask);
+
+    if(gpio_status & mask) {
+        gpio_pin_intr_state_set(GPIO_ID_PIN(s->pin_rx), GPIO_PIN_INTR_DISABLE);
+
+        level = GPIO_INPUT_GET(GPIO_ID_PIN(s->pin_rx));
         if(!level) {
             //pin is low
             //therefore we have a start bit
@@ -139,11 +92,10 @@ void softuart_intr_handler()
                     //If system timer overflow, escape from while loop
                     if ((0x7FFFFFFF & system_get_time()) < start_time){break;}
                 }
-                //shift d to the right
                 d >>= 1;
 
                 //read bit
-                if(GPIO_INPUT_GET(GPIO_ID_PIN(s->pin_rx.gpio_id))) {
+                if(GPIO_INPUT_GET(GPIO_ID_PIN(s->pin_rx))) {
                     //if high, set msb of 8bit to 1
                     d |= 0x80;
                 }
@@ -180,15 +132,19 @@ void softuart_intr_handler()
         GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status);
 
         // Reactivate interrupts for GPIO0
-        gpio_pin_intr_state_set(GPIO_ID_PIN(s->pin_rx.gpio_id), 3);
+        gpio_pin_intr_state_set(GPIO_ID_PIN(s->pin_rx), GPIO_PIN_INTR_ANYEDGE);
     } else {
         //clear interrupt, no matter from which pin
         //otherwise, this interrupt will be called again forever
         GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status);
     }
+
+    ret_gpio_status &= ~(mask);
+
+    return ret_gpio_status;
 }
 
-void softuart_init(uint32_t baudrate)
+void softuart_init()
 {
     Softuart* s = &softuart;
 
@@ -196,69 +152,29 @@ void softuart_init(uint32_t baudrate)
     gpio_init();
 
 	//set bit time
+    uint32_t baudrate = s->baudrate;
     s->bit_time = (1000000 / baudrate);
-    if ( ((100000000 / baudrate) - (100*s->bit_time)) > 50 ) s->bit_time++;
-    NODE_DBG("SOFTUART bit_time is %d\r\n",s->bit_time);
-
-    s->baudrate = baudrate;
+    if (((100000000 / baudrate) - (100*s->bit_time)) > 50 ) s->bit_time++;
+    NODE_DBG("SOFTUART bit_time is %d\r\n", s->bit_time);
 
 	//init tx pin
-	if(!s->pin_tx.gpio_mux_name) {
-		NODE_DBG("SOFTUART ERROR: Set tx pin (%d)\r\n",s->pin_tx.gpio_mux_name);
-	} else {
-		//enable pin as gpio
-    	PIN_FUNC_SELECT(s->pin_tx.gpio_mux_name, s->pin_tx.gpio_func);
+    platform_gpio_mode(s->pin_rx, PLATFORM_GPIO_OUTPUT, PLATFORM_GPIO_PULLUP);
+    GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx), 1);
+    uint32_t delay = 100000; //TODO needed?
+    os_delay_us(delay);
+    NODE_DBG("SOFTUART TX INIT DONE\r\n");
 
-		//set pullup (UART idle is VDD)
-		PIN_PULLUP_EN(s->pin_tx.gpio_mux_name);
-		
-		//set high for tx idle
-		GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx.gpio_id), 1);
-        uint32_t delay = 100000;
-		os_delay_us(delay);
+    //init rx pin
+    platform_gpio_mode(s->pin_rx, PLATFORM_GPIO_INT, PLATFORM_GPIO_PULLUP);
+    uint32_t mask = 1 << s->pin_rx;
+    platform_gpio_register_intr_hook(mask, softuart_intr_handler);
 
-		NODE_DBG("SOFTUART TX INIT DONE\r\n");
-	}
+    //clear interrupt handler status, basically writing a low to the output
+    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(s->pin_rx)); //TODO needed?
 
-	//init rx pin
-	if(!s->pin_rx.gpio_mux_name) {
-		NODE_DBG("SOFTUART ERROR: Set rx pin (%d)\r\n",s->pin_rx.gpio_mux_name);
-	} else {
-		//enable pin as gpio
-    	PIN_FUNC_SELECT(s->pin_rx.gpio_mux_name, s->pin_rx.gpio_func);
-
-		//set pullup (UART idle is VDD)
-		PIN_PULLUP_EN(s->pin_rx.gpio_mux_name);
-		
-		//set to input -> disable output
-		GPIO_DIS_OUTPUT(GPIO_ID_PIN(s->pin_rx.gpio_id));
-
-		//set interrupt related things
-
-		//disable interrupts by GPIO
-		ETS_GPIO_INTR_DISABLE();
-
-		//attach interrupt handler and a pointer that will be passed around each time
-		ETS_GPIO_INTR_ATTACH((ets_isr_t)softuart_intr_handler, NULL);
-
-		gpio_register_set(GPIO_PIN_ADDR(s->pin_rx.gpio_id), GPIO_PIN_INT_TYPE_SET(GPIO_PIN_INTR_DISABLE)  |
-                           GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_DISABLE) |
-                           GPIO_PIN_SOURCE_SET(GPIO_AS_PIN_SOURCE));
-		
-		//clear interrupt handler status, basically writing a low to the output
-		GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(s->pin_rx.gpio_id));
-
-		//enable interrupt for pin on any edge (rise and fall)
-		//@TODO: should work with ANYEDGE (=3), but complie error
-		gpio_pin_intr_state_set(GPIO_ID_PIN(s->pin_rx.gpio_id), 3);
-
-		//globally enable GPIO interrupts
-		ETS_GPIO_INTR_ENABLE();
-	
-		NODE_DBG("SOFTUART RX INIT DONE\r\n");
-	}
-
-	NODE_DBG("SOFTUART INIT DONE\r\n");
+    //enable interrupt for pin on any edge (rise and fall)
+    gpio_pin_intr_state_set(GPIO_ID_PIN(s->pin_rx), GPIO_PIN_INTR_ANYEDGE);
+    NODE_DBG("SOFTUART RX INIT DONE\r\n");
 }
 
 static inline u8 chbit(u8 data, u8 bit)
@@ -282,7 +198,7 @@ void softuart_putchar(char data)
 	unsigned start_time = 0x7FFFFFFF & system_get_time();
 
 	//Start Bit
-	GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx.gpio_id), 0);
+	GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx), 0); //TODO replay by plaform function
 	for(i = 0; i < 8; i ++ )
 	{
 		while ((0x7FFFFFFF & system_get_time()) < (start_time + (s->bit_time*(i+1))))
@@ -290,7 +206,7 @@ void softuart_putchar(char data)
 			//If system timer overflow, escape from while loop
 			if ((0x7FFFFFFF & system_get_time()) < start_time){break;}
 		}
-		GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx.gpio_id), chbit(data,1<<i));
+		GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx), chbit(data,1<<i));
 	}
 
 	// Stop bit
@@ -299,7 +215,7 @@ void softuart_putchar(char data)
 		//If system timer overflow, escape from while loop
 		if ((0x7FFFFFFF & system_get_time()) < start_time){break;}
 	}
-	GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx.gpio_id), 1);
+	GPIO_OUTPUT_SET(GPIO_ID_PIN(s->pin_tx), 1);
 
 	// Delay after byte, for new sync
 	os_delay_us(s->bit_time*6);
@@ -313,20 +229,25 @@ static int softuart_setup(lua_State* L)
     uint8_t tx_gpio_id = (uint8_t) luaL_checkinteger(L, 2);
     uint8_t rx_gpio_id = (uint8_t) luaL_checkinteger(L, 3);
 
+    if(softuart.enabled)
+        return luaL_error( L, "SOFTUART already configured." );
+
     if(!softuart_gpio_valid(tx_gpio_id)) {
         return luaL_error(L, "SOFTUART tx GPIO not valid");
     }
-    softuart_set_rx(tx_gpio_id);
+    softuart_set_tx(tx_gpio_id);
     if(!softuart_gpio_valid(rx_gpio_id)) {
         return luaL_error(L, "SOFTUART rx GPIO not valid");
     }
-    softuart_set_tx(rx_gpio_id);
+    softuart_set_rx(rx_gpio_id);
 
     if (baudrate <= 0 || baudrate > 38400)
     {
         return luaL_error(L, "SOFTUART invalid baud rate" );
     }
-    softuart_init(baudrate);
+    softuart.baudrate = baudrate;
+
+    softuart_init();
 
     softuart.uart_receive_rf = LUA_NOREF;
     softuart.enabled = true;
--- lua_examples/softuart.lua
+++ lua_examples/softuart.lua
@@ -4,7 +4,8 @@
 -- -- Connect a cable from gpio5 to gipio2/txd1 to receive the data via softuart.
 --
 
-softuart.setup(9600, 5, 4)
+softuart.setup(9600, 4, 5)
 softuart.on("data", 1, function(data) print("lua handler called"); print(data) end)
 uart.setup(1, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1)
 uart.write(1, "1234567890\n01234567890")
+softuart.write("test")
