15d4311f40e046bc7b47831d9ee722a856177aab
devsaurus <devsaurus@users.noreply.github.com>
2017-11-19 22:42:20 +0100

    add docs

--- app/include/u8g2_fonts.h
+++ app/include/u8g2_fonts.h
@@ -4,9 +4,15 @@
 
 #define U8G2_FONT_TABLE_ENTRY(font)
 
+// ***************************************************************************
+// Configure U8glib fonts
+//
+// Add a U8G2_FONT_TABLE_ENTRY for each font you want to compile into the image
 #define U8G2_FONT_TABLE \
   U8G2_FONT_TABLE_ENTRY(font_6x10_tf) \
   U8G2_FONT_TABLE_ENTRY(font_unifont_t_symbols) \
+//
+// ***************************************************************************
 
 
 #endif /* _U8G2_FONTS_H */
