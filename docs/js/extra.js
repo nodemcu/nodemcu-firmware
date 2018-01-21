316a5fc566077b9f10923969a345535aac26b6da
Marcel Stör <marcelstoer@users.noreply.github.com>
2018-01-21 23:58:58 +0100

    With RTD the JS search seems no longer necessary

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -8,7 +8,6 @@ var nodemcu = nodemcu || {};
 
   $(document).ready(function () {
     addToc();
-    fixSearch();
     hideNavigationForAllButSelectedLanguage();
     addLanguageSelectorToRtdFlyOutMenu();
     replaceRelativeLinksWithStaticGitHubUrl();
@@ -42,34 +41,6 @@ var nodemcu = nodemcu || {};
       var href = func.replace(/[\.:\(\)]/g, '').replace(/ --|, | /g, '-');
       var link = '<a href="#' + href.toLowerCase() + '">' + func + '</a>';
       return '<tr><td>' + link + '</td><td>' + intro + '</td></tr>';
-    }
-  }
-
-  /*
-   * RTD messes up MkDocs' search feature by tinkering with the search box defined in the theme, see
-   * https://github.com/rtfd/readthedocs.org/issues/1088. This function sets up a DOM4 MutationObserver
-   * to react to changes to the search form (triggered by RTD on doc ready). It then reverts everything
-   * the RTD JS code modified.
-   */
-  function fixSearch() {
-    var target = document.getElementById('rtd-search-form');
-    var config = {attributes: true, childList: true};
-
-    var observer = new MutationObserver(function(mutations) {
-      // if it isn't disconnected it'll loop infinitely because the observed element is modified
-      observer.disconnect();
-      var form = $('#rtd-search-form');
-      form.empty();
-      form.attr('action', 'https://' + window.location.hostname + '/en/' + determineSelectedBranch() + '/search.html');
-      $('<input>').attr({
-        type: "text",
-        name: "q",
-        placeholder: "Search docs"
-      }).appendTo(form);
-    });
-
-    if (window.location.origin.indexOf('readthedocs') > -1) {
-      observer.observe(target, config);
     }
   }
 
