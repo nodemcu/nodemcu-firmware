2e6f95fe8e9469122396b149fa7995517b602ab4
hsj1104 <39845210+hsj1104@users.noreply.github.com>
2018-06-02 09:55:48 +0800

    Update coap.c

--- app/modules/coap.c
+++ app/modules/coap.c
@@ -31,6 +31,7 @@ typedef struct lcoap_userdata
 static void coap_received(void *arg, char *pdata, unsigned short len)
 {
   NODE_DBG("coap_received is called.\n");
+  printf("pdata is %s\n",pdata);
   struct espconn *pesp_conn = arg;
   lcoap_userdata *cud = (lcoap_userdata *)pesp_conn->reverse;
 
