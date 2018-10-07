509be837a839f86fc12514abdc568aa61e1805d7
Arnim Läuger <devsaurus@users.noreply.github.com>
2018-10-07 22:46:56 +0200

    Update u8g2 to v2.23.18 (#2500)
    * Update u8g2 to v2.23.18
    * serve wdt for long delays

--- app/platform/u8x8_nodemcu_hal.c
+++ app/platform/u8x8_nodemcu_hal.c
@@ -50,6 +50,7 @@ uint8_t u8x8_gpio_and_delay_nodemcu(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, 
 
   case U8X8_MSG_DELAY_MILLI:          // delay arg_int * 1 milli second
     os_delay_us( arg_int * 1000 );
+    system_soft_wdt_feed();
     break;
 
   case U8X8_MSG_DELAY_I2C:                // arg_int is the I2C speed in 100KHz, e.g. 4 = 400 KHz
