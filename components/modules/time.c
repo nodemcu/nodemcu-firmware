e05fd26e7b78cf4c36a4959e46b4c0a5c2c5adb5
Skirmantas Lazikas <skirmantas.lauzikas@blackraven.lt>
2018-11-25 06:25:24 +0200

    ESP32: Time module documentation & style fixes * added documentation for time modules * style fixes as pointed out by @devsaurus

--- components/modules/time.c
+++ components/modules/time.c
@@ -10,95 +10,95 @@
 
 
 #define ADD_TABLE_ITEM(L, key, val) \
-	lua_pushinteger (L, val);	    \
-	lua_setfield (L, -2, key);
+  lua_pushinteger (L, val);      \
+  lua_setfield (L, -2, key);
+  
+void inline time_tmToTable(lua_State *L, struct tm *date)
+{ 
+  lua_createtable (L, 0, 9);
+  
+  ADD_TABLE_ITEM (L, "yday", date->tm_yday + 1);
+  ADD_TABLE_ITEM (L, "wday", date->tm_wday + 1);
+  ADD_TABLE_ITEM (L, "year", date->tm_year + 1900);
+  ADD_TABLE_ITEM (L, "mon",  date->tm_mon + 1);
+  ADD_TABLE_ITEM (L, "day",  date->tm_mday);
+  ADD_TABLE_ITEM (L, "hour", date->tm_hour);
+  ADD_TABLE_ITEM (L, "min",  date->tm_min);
+  ADD_TABLE_ITEM (L, "sec",  date->tm_sec);
+  ADD_TABLE_ITEM (L, "dst",  date.tm_isdst);
+}
 
 static int time_get(lua_State *L)
 {
-	struct timeval  tv;
-	gettimeofday (&tv, NULL);
-	lua_pushnumber (L, tv.tv_sec);
-	lua_pushnumber (L, tv.tv_usec);
-	return 2;
+  struct timeval  tv;
+  gettimeofday (&tv, NULL);
+  lua_pushnumber (L, tv.tv_sec);
+  lua_pushnumber (L, tv.tv_usec);
+  return 2;
 }
 
 static int time_set(lua_State *L)
 {
-	uint32_t sec = luaL_checknumber (L, 1);
-	uint32_t usec = 0;
-	if (lua_isnumber (L, 2))
-		usec = lua_tonumber (L, 2);
+  uint32_t sec = luaL_checknumber (L, 1);
+  uint32_t usec = 0;
+  if (lua_isnumber (L, 2))
+    usec = lua_tonumber (L, 2);
 
-	struct timeval tv = { sec, usec };
-	settimeofday (&tv, NULL);
+  struct timeval tv = { sec, usec };
+  settimeofday (&tv, NULL);
 
-	return 0;
+  return 0;
 }
 
 static int time_getLocal(lua_State *L)
 {
-	time_t now;
-	struct tm date;
-	
-	time(&now);	
-	localtime_r(&now, &date);
-	
-	  /* construct Lua table */
-	lua_createtable (L, 0, 8);
-	
-	ADD_TABLE_ITEM (L, "yday", date.tm_yday + 1);
-	ADD_TABLE_ITEM (L, "wday", date.tm_wday + 1);
-	ADD_TABLE_ITEM (L, "year", date.tm_year + 1900);
-	ADD_TABLE_ITEM (L, "mon",  date.tm_mon + 1);
-	ADD_TABLE_ITEM (L, "day",  date.tm_mday);
-	ADD_TABLE_ITEM (L, "hour", date.tm_hour);
-	ADD_TABLE_ITEM (L, "min",  date.tm_min);
-	ADD_TABLE_ITEM (L, "sec",  date.tm_sec);
-	ADD_TABLE_ITEM (L, "dst",  date.tm_isdst);
+  time_t now;
+  struct tm date;
+  
+  time(&now);  
+  localtime_r(&now, &date);
+  
+    /* construct Lua table */
+  time_tmToTable(L, &date);
 
-	return 1;
+  return 1;
 }
 
 static int time_setTimezone(lua_State *L)
 {
-	size_t l;
-	const char *timezone = luaL_checklstring(L, 1, &l);
-	
-	setenv("TZ", timezone, 1);
-	tzset();
-	
-	return 0;
+  size_t l;
+  const char *timezone = luaL_checklstring(L, 1, &l);
+  
+  setenv("TZ", timezone, 1);
+  tzset();
+  
+  return 0;
 }
 
 static int time_initNTP(lua_State *L)
 {
-	size_t l;
-	const char *server = luaL_checklstring(L, 1, &l);
-	
-	if(l < 6)
-	{
-		server = "pool.ntp.org";	
-	}
-	
-	sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, server);
-	sntp_init();
-	
-	return 0;
+  size_t l;
+  const char *server = luaL_checklstring(L, 1, "pool.ntp.org", &l);
+  
+  sntp_setoperatingmode(SNTP_OPMODE_POLL);
+  sntp_setservername(0, server);
+  sntp_init();
+  
+  return 0;
 }
 
 static int time_ntpEnabled(lua_State *L)
 {
-	lua_pushboolean(L, sntp_enabled());
-	
-	return 1;
+  lua_pushboolean(L, sntp_enabled());
+  
+  return 1;
 }
 
 static int time_ntpStop(lua_State *L)
 {
-	sntp_stop();
-	
-	return 0;
+  sntp_stop();
+  
+  return 0;
 }
 
 static int time_epoch2cal(lua_State *L)
@@ -111,58 +111,49 @@ static int time_epoch2cal(lua_State *L)
   date = gmtime (&now);
 
   /* construct Lua table */
-  lua_createtable (L, 0, 8);
-  ADD_TABLE_ITEM (L, "yday", date->tm_yday + 1);
-  ADD_TABLE_ITEM (L, "wday", date->tm_wday + 1);
-  ADD_TABLE_ITEM (L, "year", date->tm_year + 1900);
-  ADD_TABLE_ITEM (L, "mon",  date->tm_mon + 1);
-  ADD_TABLE_ITEM (L, "day",  date->tm_mday);
-  ADD_TABLE_ITEM (L, "hour", date->tm_hour);
-  ADD_TABLE_ITEM (L, "min",  date->tm_min);
-  ADD_TABLE_ITEM (L, "sec",  date->tm_sec);
+  time_tmToTable(L, &date);
 
   return 1;
 }
 
 static int time_cal2epoc(lua_State *L)
 {
-	struct tm date;
-	luaL_checkanytable (L, 1);
-	
-	lua_getfield (L, 1, "year");
-	date.tm_year = luaL_optinteger(L, -1, 1900) - 1900;
-	
-	lua_getfield (L, 1, "mon");
-	date.tm_mon = luaL_optinteger(L, -1, 1) - 1;
-	
-	lua_getfield (L, 1, "day");
-	date.tm_mday = luaL_optinteger(L, -1, 0);
-	
-	lua_getfield (L, 1, "hour");
-	date.tm_hour = luaL_optinteger(L, -1, 0);
-	
-	lua_getfield (L, 1, "min");
-	date.tm_min = luaL_optinteger(L, -1, 0);
-	
-	lua_getfield (L, 1, "sec");
-	date.tm_sec = luaL_optinteger(L, -1, 0);
-	
-	lua_pushnumber (L, mktime(&date));
-	
-	return 1;
+  struct tm date;
+  luaL_checkanytable (L, 1);
+  
+  lua_getfield (L, 1, "year");
+  date.tm_year = luaL_optinteger(L, -1, 1900) - 1900;
+  
+  lua_getfield (L, 1, "mon");
+  date.tm_mon = luaL_optinteger(L, -1, 1) - 1;
+  
+  lua_getfield (L, 1, "day");
+  date.tm_mday = luaL_optinteger(L, -1, 0);
+  
+  lua_getfield (L, 1, "hour");
+  date.tm_hour = luaL_optinteger(L, -1, 0);
+  
+  lua_getfield (L, 1, "min");
+  date.tm_min = luaL_optinteger(L, -1, 0);
+  
+  lua_getfield (L, 1, "sec");
+  date.tm_sec = luaL_optinteger(L, -1, 0);
+  
+  lua_pushnumber (L, mktime(&date));
+  
+  return 1;
 }
 
 static const LUA_REG_TYPE time_map[] = {
-  { LSTRKEY("set"),            	LFUNCVAL(time_set) },
-  { LSTRKEY("get"),            	LFUNCVAL(time_get) },
-  { LSTRKEY("getlocal"),       	LFUNCVAL(time_getLocal) },
-  { LSTRKEY("settimezone"),   	LFUNCVAL(time_setTimezone) },
-  { LSTRKEY("initntp"),         LFUNCVAL(time_initNTP)  },
-  { LSTRKEY("ntpenabled"),      LFUNCVAL(time_ntpEnabled) },
-  { LSTRKEY("ntpstop"),         LFUNCVAL(time_ntpStop) },
-  { LSTRKEY("epoch2cal"),      	LFUNCVAL(time_epoch2cal) },
-  { LSTRKEY("cal2epoch"),      	LFUNCVAL(time_cal2epoc) },
-  
+  { LSTRKEY("set"),              LFUNCVAL(time_set) },
+  { LSTRKEY("get"),              LFUNCVAL(time_get) },
+  { LSTRKEY("getlocal"),         LFUNCVAL(time_getLocal) },
+  { LSTRKEY("settimezone"),      LFUNCVAL(time_setTimezone) },
+  { LSTRKEY("initntp"),          LFUNCVAL(time_initNTP)  },
+  { LSTRKEY("ntpenabled"),       LFUNCVAL(time_ntpEnabled) },
+  { LSTRKEY("ntpstop"),          LFUNCVAL(time_ntpStop) },
+  { LSTRKEY("epoch2cal"),        LFUNCVAL(time_epoch2cal) },
+  { LSTRKEY("cal2epoch"),        LFUNCVAL(time_cal2epoc) },
   { LNILKEY, LNILVAL }
 };
 
