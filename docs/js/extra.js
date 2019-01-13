46028b25f99184b49895e96aaa7302c79e270862
Marcel Stör <marcelstoer@users.noreply.github.com>
2019-01-13 18:10:21 +0100

    Fix relative path to firmware sources

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -43,7 +43,7 @@ var nodemcu = nodemcu || {};
    * replaces the relative path with an absolute path based on the selected branch.
    */
   function replaceRelativeLinksWithStaticGitHubUrl() {
-    var relativePath = "../../../..";
+    var relativePath = "../../..";
     var gitHubPath = "https://github.com/nodemcu/nodemcu-firmware/tree/" + determineSelectedBranch();
     var gitHubLinks = $("a[href^='" + relativePath + "']").each(function (index) {
       var url = $(this).attr('href');
