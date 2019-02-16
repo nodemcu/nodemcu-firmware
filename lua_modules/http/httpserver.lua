dcc1ea2a49c508499c330faa37d31c2994bef51f
Nathaniel Wesley Filardo <VP331RHQ115POU58JFRLKB7OPA0L18E3@cmx.ietfng.org>
2019-02-16 12:51:40 +0000

    A generic fifo and fifosock wrapper, under telnet and http server (#2650)
    * lua_modules/fifo: a generic queue & socket wrapper
    
    One occasionally wants a generic fifo, so here's a plausible
    implementation that's reasonably flexible in its usage.
    
    One possible consumer of this is a variant of TerryE's two-level fifo
    trick currently in the telnetd example.  Factor that out to fifosock for
    more general use.
    
    * lua_examples/telnet: use factored out fifosock
    
    * lua_modules/http: improve implementation
    
    Switch to fifosock for in-order sending and waiting for everything to be
    sent before closing.
    
    Fix header callback by moving the invocation of the handler higher
    
    * fifosock: optimistically cork and delay tx
    
    If we just pushed a little bit of data into a fifosock that had idled,
    wait a tick (1 ms) before transmitting.  Hopefully, this means that
    we let the rest of the system push more data in before we send the first
    packet.  But in a high-throughput situation, where we are streaming data
    without idling the fifo, there won't be any additional delay and we'll
    coalesce during operation as usual.
    
    The fifosocktest mocks up enough of tmr for this to run, but assumes
    an arbitrarily slow processor. ;)

--- lua_modules/http/httpserver.lua
+++ lua_modules/http/httpserver.lua
@@ -12,27 +12,24 @@ do
   -- request methods
   ------------------------------------------------------------------------------
   local make_req = function(conn, method, url)
-    local req = {
+    return {
       conn = conn,
       method = method,
       url = url,
     }
-    -- return setmetatable(req, {
-    -- })
-    return req
   end
 
   ------------------------------------------------------------------------------
   -- response methods
   ------------------------------------------------------------------------------
-  local send = function(self, data, status)
-    local c = self.conn
+  local make_res = function(csend, cfini)
+   local send = function(self, data, status)
     -- TODO: req.send should take care of response headers!
     if self.send_header then
-      c:send("HTTP/1.1 ")
-      c:send(tostring(status or 200))
+      csend("HTTP/1.1 ")
+      csend(tostring(status or 200))
       -- TODO: real HTTP status code/name table
-      c:send(" OK\r\n")
+      csend(" OK\r\n")
       -- we use chunked transfer encoding, to not deal with Content-Length:
       --   response header
       self:send_header("Transfer-Encoding", "chunked")
@@ -43,55 +40,51 @@ do
       if self.send_header then
         self.send_header = nil
         -- end response headers
-        c:send("\r\n")
+        csend("\r\n")
       end
       -- chunked transfer encoding
-      c:send(("%X\r\n"):format(#data))
-      c:send(data)
-      c:send("\r\n")
+      csend(("%X\r\n"):format(#data))
+      csend(data)
+      csend("\r\n")
     end
-  end
-  local send_header = function(self, name, value)
-    local c = self.conn
+   end
+   local send_header = function(self, name, value)
     -- NB: quite a naive implementation
-    c:send(name)
-    c:send(": ")
-    c:send(value)
-    c:send("\r\n")
-  end
-  -- finalize request, optionally sending data
-  local finish = function(self, data, status)
-    local c = self.conn
-    -- NB: req.send takes care of response headers
+    csend(name)
+    csend(": ")
+    csend(value)
+    csend("\r\n")
+   end
+   -- finalize request, optionally sending data
+   local finish = function(self, data, status)
+    -- NB: res.send takes care of response headers
     if data then
       self:send(data, status)
     end
     -- finalize chunked transfer encoding
-    c:send("0\r\n\r\n")
+    csend("0\r\n\r\n")
     -- close connection
-    c:close()
-  end
-  --
-  local make_res = function(conn)
-    local res = {
-      conn = conn,
-    }
-    -- return setmetatable(res, {
-    --  send_header = send_header,
-    --  send = send,
-    --  finish = finish,
-    -- })
+    cfini()
+   end
+   --
+    local res = { }
     res.send_header = send_header
     res.send = send
     res.finish = finish
     return res
   end
 
-    ------------------------------------------------------------------------------
+  ------------------------------------------------------------------------------
   -- HTTP parser
   ------------------------------------------------------------------------------
   local http_handler = function(handler)
     return function(conn)
+      local csend = (require "fifosock")(conn)
+      local cfini = function()
+        conn:on("receive", nil)
+        conn:on("disconnection", nil)
+        csend(function() conn:on("sent", nil) conn:close() end)
+      end
       local req, res
       local buf = ""
       local method, url
@@ -108,7 +101,7 @@ do
           cnt_len = tonumber(v)
         end
         if k == "expect" and v == "100-continue" then
-          conn:send("HTTP/1.1 100 Continue\r\n")
+          csend("HTTP/1.1 100 Continue\r\n")
         end
         -- delegate to request object
         if req and req.onheader then
@@ -118,8 +111,6 @@ do
       -- body data handler
       local body_len = 0
       local ondata = function(conn, chunk)
-        -- NB: do not reset node in case of lengthy requests
-        tmr.wdclr()
         -- feed request data to request handler
         if not req or not req.ondata then return end
         req:ondata(chunk)
@@ -153,8 +144,10 @@ do
             if method then
               -- make request and response objects
               req = make_req(conn, method, url)
-              res = make_res(conn)
+              res = make_res(csend, cfini)
             end
+            -- spawn request handler
+            handler(req, res)
           -- header line?
           elseif #line > 0 then
             -- parse header
@@ -166,11 +159,6 @@ do
             end
           -- headers end
           else
-            -- spawn request handler
-            -- NB: do not reset in case of lengthy requests
-            tmr.wdclr()
-            handler(req, res)
-            tmr.wdclr()
             -- NB: we feed the rest of the buffer as starting chunk of body
             ondata(conn, buf)
             -- buffer no longer needed
