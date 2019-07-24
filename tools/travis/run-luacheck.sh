be812232b03fc3f012fa3b98b4bd77e5f530faba
Galion <galion@sdf.org>
2019-07-24 14:04:01 +0200

    Speed up Travis by using preinstalled LuaRocks

--- tools/travis/run-luacheck.sh
+++ tools/travis/run-luacheck.sh
@@ -2,16 +2,71 @@
 
 set -e
 
-echo "Installing Lua 5.3, LuaRocks and Luacheck"
-(
-  cd "$TRAVIS_BUILD_DIR" || exit
+exists() {
+  if command -v "$1" >/dev/null 2>&1
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+usage() {
+echo "
+usage: bash tools/travis/run-luacheck.sh [-s]
+
+Avarible options are:
+-s:		Standalone mode: Lua, LuaRocks and luacheck
+will be installed in nodemcu-firmare/cache folder.
+
+By default script will use luarocks installed in host system.
+"
+}
+
+install_tools() {
+  if ! exists luarocks; then
+    echo "LuaRocks not found!"
+    exit 1
+  fi
+
+  eval "`luarocks path --bin`"  #Set PATH for luacheck
+  #In Travis Path it's not changed by LuaRocks for some unknown reason
+  if [ "${TRAVIS}" = "true" ]; then 
+  	export PATH=$PATH:/home/travis/.luarocks/bin
+  fi
+
+  if ! exists luacheck; then
+  	echo "Installing luacheck"
+  	luarocks install --local luacheck || exit
+  fi
+  
+}
+
+install_tools_standalone() {
+  if ! [ -x cache/localua/bin/luarocks ]; then
+  echo "Installing Lua 5.3 and LuaRocks"
   bash tools/travis/localua.sh cache/localua || exit
-  cache/localua/bin/luarocks install luacheck || exit
-)
+  fi
 
-(
-  echo "Static analysys of:"
-  find lua_modules lua_examples -iname "*.lua" -print0 | xargs -0 echo
-)
+  if ! [ -x cache/localua/bin/luacheck ]; then
+    echo "Installing luacheck"
+    cache/localua/bin/luarocks install luacheck || exit
+  fi
+}
 
-(find lua_modules lua_examples -iname "*.lua" -print0 | xargs -0 cache/localua/bin/luacheck --config tools/luacheck_config.lua) || exit
+if [[ $1 == "" ]]; then
+  	install_tools
+  else
+  while getopts "s" opt
+  do
+    case $opt in
+    (s) install_tools_standalone ;;
+    (*) usage; exit 1 ;;
+    esac
+  done
+fi
+
+echo "Static analysys of"
+find lua_modules lua_examples -iname "*.lua" -print0 | xargs -0 echo
+
+(find lua_modules lua_examples -iname "*.lua" -print0 | xargs -0 luacheck --config tools/luacheck_config.lua) || exit
