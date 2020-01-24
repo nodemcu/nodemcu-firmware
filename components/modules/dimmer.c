3a6f44c1a69e016f01bbd6e5285b34f826140a5d
Javier Peletier <jm@epiclabs.io>
2020-01-24 15:39:43 +0100

    messages

--- components/modules/dimmer.c
+++ components/modules/dimmer.c
@@ -26,116 +26,92 @@ typedef struct {
     bool switched;
 } dim_t;
 
+typedef enum {
+    MT_IDLE = 0,
+    MT_ADD = 1,
+    MT_REMOVE = 2,
+    MT_SETLEVEL = 3,
+} message_type_t;
+
 typedef struct {
-    esp_err_t err;
-} config_interrupt_t;
+    message_type_t messageType;
+    int pin;
+    union {
+        int mode;
+        uint32_t level;
+        esp_err_t err;
+    };
 
-#define DEBOUNCE_CYCLES 400000  // good for any CPU and mains frequency
+} dimmer_message_t;
 
-#define DIM_TIMER0 0x0
-#define DIM_TIMER1 0x1
-#define DIM_TIMER2 0x2
-#define DIM_TIMER3 0x3
 #define DIM_MODE_LEADING_EDGE 0x0
 #define DIM_MODE_TRAILING_EDGE 0x1
+#define STACK_SIZE 4096
 
 // menuconfig changes required in components/ESP32-specific...
 // removed: "Also watch CPU1 tick interrupt"
 // removed: "Watch CPU1 idle task"
 
-static volatile bool disable_loop = false;
-static volatile bool disable_loop_ack = false;
+static volatile dimmer_message_t message = {.messageType = MT_IDLE};
 static volatile int zCount = 0;
 static int zcPin = -1;
 static volatile uint32_t zcTimestamp = 0;
 static uint32_t p = 0;
 static dim_t* dims = NULL;
 static int dimCount = 0;
+StaticTask_t xTaskBuffer;
 
-static int check_err(lua_State* L, esp_err_t err) {
+static void check_err(lua_State* L, esp_err_t err) {
     switch (err) {
         case ESP_ERR_INVALID_ARG:
             luaL_error(L, "invalid argument");
         case ESP_ERR_INVALID_STATE:
             luaL_error(L, "internal logic error");
+        case ESP_ERR_NO_MEM:
+            luaL_error(L, "out of memory");
         case ESP_OK:
             break;
+        default:
+            luaL_error(L, "Error code %d", err);
     }
-    return 0;
 }
 
-static void enable_interrupts() {
-    disable_loop = false;
-    disable_loop_ack = false;
-}
-
-static void disable_interrupts() {
-    disable_loop = true;
-    while (!disable_loop_ack)
-        ;
-}
-
-// pin
-static int dimmer_add(lua_State* L) {
-    int pin = luaL_checkint(L, 1);
-    int mode = luaL_optint(L, 2, DIM_MODE_LEADING_EDGE);
+static inline esp_err_t IRAM_ATTR msg_add_dimmer() {
     for (int i = 0; i < dimCount; i++) {
-        if (dims[i].pin == pin) {
-            return 0;
+        if (dims[i].pin == message.pin) {
+            return ESP_OK;
         }
     }
-    check_err(L, gpio_set_direction(pin, GPIO_MODE_OUTPUT));
-    disable_interrupts();
-    dims = luaM_realloc_(L, dims, dimCount * sizeof(dim_t), (dimCount + 1) * sizeof(dim_t));
-    dims[dimCount].pin = pin;
-    dims[dimCount].level = (mode == DIM_MODE_LEADING_EDGE) ? p * 2 : 0;
-    dims[dimCount].mode = mode;
+    dims = realloc(dims, (dimCount + 1) * sizeof(dim_t));
+    if (dims == NULL) {
+        return ESP_ERR_NO_MEM;
+    }
+    dims[dimCount].pin = message.pin;
+    dims[dimCount].level = (message.mode == DIM_MODE_LEADING_EDGE) ? p * 2 : 0;
+    dims[dimCount].mode = message.mode;
     dims[dimCount].switched = false;
     dimCount++;
-    gpio_set_level(pin, 0);
-    enable_interrupts();
-    return 0;
+    return ESP_OK;
 }
 
-static int dimmer_remove(lua_State* L) {
-    int pin = luaL_checkint(L, 1);
-    disable_interrupts();
+static inline esp_err_t IRAM_ATTR msg_remove_dimmer() {
     for (int i = 0; i < dimCount; i++) {
-        if (dims[i].pin == pin) {
+        if (dims[i].pin == message.pin) {
             for (int j = i; j < dimCount - 1; j++) {
                 dims[j] = dims[j + 1];
             }
-            dims = luaM_realloc_(L, dims, dimCount * sizeof(dim_t), (dimCount - 1) * sizeof(dim_t));
+            dims = realloc(dims, (dimCount - 1) * sizeof(dim_t));
             dimCount--;
-            enable_interrupts();
-            return 0;
+            return ESP_OK;
         }
     }
-    enable_interrupts();
-    luaL_error(L, "Error: pin %d is not dimmed.", pin);
-    return 0;
+    return ESP_ERR_INVALID_ARG;
 }
 
-static int dimmer_list_debug(lua_State* L) {
-    ESP_LOGW(TAG, "p=%u, zcount=%d, zcTimestamp=%u, esp_freq=%d", p, zCount, zcTimestamp, esp_clk_cpu_freq());
-    disable_interrupts();
-    int dc = dimCount;
-    dim_t dimsc[dimCount];
-    memcpy(dimsc, dims, sizeof(dim_t) * dimCount);
-    enable_interrupts();
-
-    for (int i = 0; i < dc; i++) {
-        ESP_LOGW(TAG, "pin=%d, mode=%d, level=%u", dimsc[i].pin, dimsc[i].mode, dimsc[i].level);
-    }
-    return 0;
-}
-
-static int dimmer_set_level(lua_State* L) {
-    int pin = luaL_checkint(L, 1);
-
+static inline esp_err_t IRAM_ATTR msg_set_level() {
     for (int i = 0; i < dimCount; i++) {
-        if (dims[i].pin == pin) {
-            int level = luaL_checkint(L, 2);
+        if (dims[i].pin == message.pin) {
+            int level = message.level;
             if (dims[i].mode == DIM_MODE_LEADING_EDGE) {
                 level = 1000 - level;
             }
@@ -146,21 +122,10 @@ static int dimmer_set_level(lua_State* L) {
             } else {
                 dims[i].level = (uint32_t)((((double)level) / 1000.0) * ((double)p));
             }
-            return 0;
+            return ESP_OK;
         }
     }
-
-    luaL_error(L, "Cannot set dim level of unconfigured pin %d", pin);
-    return 0;
-}
-
-static int dimmer_mainsFrequency(lua_State* L) {
-    int mainsF = 0;
-    if (p > 0) {
-        mainsF = (int)((esp_clk_cpu_freq() * 50.0) / p);
-    }
-    lua_pushinteger(L, mainsF);
-    return 1;
+    return ESP_ERR_INVALID_ARG;
 }
 
 static void IRAM_ATTR disable_timer() {
@@ -170,7 +135,6 @@ static void IRAM_ATTR disable_timer() {
 }
 
 static void IRAM_ATTR stuff(void* parms) {
-    //Find a way to disable the cpu1 tick interrupt.
     disable_timer();
     rtc_wdt_protect_off();
     rtc_wdt_disable();
@@ -183,11 +147,23 @@ static void IRAM_ATTR stuff(void* parms) {
     uint32_t target = 0;
 
     while (true) {
-        if (disable_loop) {
-            disable_loop_ack = true;
-            while (!disable_loop)
-                ;
+        if (message.messageType != MT_IDLE) {
+            switch (message.messageType) {
+                case MT_ADD:
+                    message.err = msg_add_dimmer();
+                    break;
+                case MT_REMOVE:
+                    message.err = msg_remove_dimmer();
+                    break;
+                case MT_SETLEVEL:
+                    message.err = msg_set_level();
+                    break;
+                default:
+                    break;
+            }
+            message.messageType = MT_IDLE;
         }
+
         // now contains the current value of the CPU cycle counter
         uint32_t volatile now = xthal_get_ccount();
         // elapsed contains the number of CPU cycles since the last zero crossing
@@ -232,11 +208,59 @@ static void IRAM_ATTR stuff(void* parms) {
     }
 }
 
-#define STACK_SIZE 4096
+static void awaitAck(lua_State* L) {
+    while (message.messageType != MT_IDLE)
+        ;
+    check_err(L, message.err);
+}
 
-StaticTask_t xTaskBuffer;
+// pin
+static int dimmer_add(lua_State* L) {
+    message.pin = luaL_checkint(L, 1);
+    message.mode = luaL_optint(L, 2, DIM_MODE_LEADING_EDGE);
+    check_err(L, gpio_set_direction(message.pin, GPIO_MODE_OUTPUT));
+    gpio_set_level(message.pin, 0);
 
-// hw timer group, hw_timer num, zc pin
+    message.messageType = MT_ADD;
+    awaitAck(L);
+
+    return 0;
+}
+
+static int dimmer_remove(lua_State* L) {
+    message.pin = luaL_checkint(L, 1);
+    message.messageType = MT_REMOVE;
+    awaitAck(L);
+    return 0;
+}
+
+static int dimmer_list_debug(lua_State* L) {
+    ESP_LOGW(TAG, "p=%u, zcount=%d, zcTimestamp=%u, esp_freq=%d", p, zCount, zcTimestamp, esp_clk_cpu_freq());
+
+    for (int i = 0; i < dimCount; i++) {
+        ESP_LOGW(TAG, "pin=%d, mode=%d, level=%u", dims[i].pin, dims[i].mode, dims[i].level);
+    }
+    return 0;
+}
+
+static int dimmer_set_level(lua_State* L) {
+    message.pin = luaL_checkint(L, 1);
+    message.level = luaL_checkint(L, 2);
+    message.messageType = MT_SETLEVEL;
+    awaitAck(L);
+
+    return 0;
+}
+
+static int dimmer_mainsFrequency(lua_State* L) {
+    int mainsF = 0;
+    if (p > 0) {
+        mainsF = (int)((esp_clk_cpu_freq() * 50.0) / p);
+    }
+    lua_pushinteger(L, mainsF);
+    return 1;
+}
+
 static int dimmer_setup(lua_State* L) {
     zcPin = luaL_checkinteger(L, -1);
 
@@ -267,11 +291,6 @@ LROT_FUNCENTRY(remove, dimmer_remove)
 LROT_FUNCENTRY(setLevel, dimmer_set_level)
 LROT_FUNCENTRY(list, dimmer_list_debug)
 LROT_FUNCENTRY(mainsFrequency, dimmer_mainsFrequency)
-LROT_NUMENTRY(TIMER_0, DIM_TIMER0)
-LROT_NUMENTRY(TIMER_1, DIM_TIMER1)
-LROT_NUMENTRY(TIMER_2, DIM_TIMER2)
-LROT_NUMENTRY(TIMER_3, DIM_TIMER3)
-
 LROT_END(dimmer, NULL, 0)
 
 int luaopen_dimmer(lua_State* L) {
