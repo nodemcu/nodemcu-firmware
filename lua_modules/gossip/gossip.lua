5045df9f81bfb96c3ef12cbeeeed685a4fdf0cfe
Alex <alexandru.antochi@gmail.com>
2020-02-09 22:13:13 +0000

    fixed some bugs, split diff and update into two methods

--- lua_modules/gossip/gossip.lua
+++ lua_modules/gossip/gossip.lua
@@ -8,6 +8,11 @@ local state = {};
 
 -- Utils
 
+utils.contains = function(list, element)
+  for k in pairs(list) do if list[k] == element then return true; end end
+  return false;
+end
+
 utils.debug = function(message)
   if gossip.config.debug then
     if gossip.config.debugOutput then
@@ -19,10 +24,11 @@ utils.debug = function(message)
 end
 
 utils.getNetworkState = function() return sjson.encode(gossip.networkState); end
-utils.getSeedList = function() return sjson.encode(gossip.config.seedList); end
+
 
 utils.isNodeDataValid = function(nodeData)
-  return (nodeData and nodeData.revision and nodeData.heartbeat and nodeData.state) ~= nil;
+  return (nodeData and nodeData.revision and nodeData.heartbeat and
+             nodeData.state) ~= nil;
 end
 
 utils.compare = function(first, second)
@@ -34,12 +40,10 @@ end
 utils.compareNodeData = function(first, second)
   local firstDataValid = utils.isNodeDataValid(first);
   local secondDataValid = utils.isNodeDataValid(second);
-
   if firstDataValid and secondDataValid then
     for index in ipairs(constants.comparisonFields) do
-      local comparisonResult = utils.compare(
-                                   first[constants.comparisonFields[index]],
-                                   second[constants.comparisonFields[index]]);
+      local comparisonField = constants.comparisonFields[index];
+      local comparisonResult = utils.compare(first[comparisonField], second[comparisonField]);
       if comparisonResult ~= 0 then return comparisonResult; end
     end
   elseif firstDataValid then
@@ -50,24 +54,21 @@ utils.compareNodeData = function(first, second)
   return 0;
 end
 
-utils.getUpdateDiffDelta = function(synData)
+-- computes data1 - data2 based on node compare function
+utils.getMinus = function(data1, data2)
   local diff = {};
-  local update = {};
-  for ip, nodeData in pairs(gossip.networkState) do
-    if utils.compareNodeData(nodeData, synData[ip]) == -1 then
-      diff[ip] = nodeData;
-    elseif utils.compareNodeData(nodeData, synData[ip]) == 1 then
-      update[ip] = synData[ip];
+  for ip, nodeData1 in pairs(data1) do
+    if utils.compareNodeData(nodeData1, data2[ip]) == -1 then
+      diff[ip] = nodeData1;
     end
   end
-  return update, diff
+  return diff;
 end
 
 utils.setConfig = function(userConfig)
   for k, v in pairs(userConfig) do
-    if gossip.config[k] and type(gossip.config[k]) == type(v) then
+    if gossip.config[k] ~= nil and type(gossip.config[k]) == type(v) then
       gossip.config[k] = v;
-      utils.debug('Set value for ' .. k);
     end
   end
 end
@@ -75,19 +76,19 @@ end
 -- State
 
 state.setRev = function(revNumber)
-  local revision = revNumber or 0;
-  if not revNumber and file.exists(revFile) then
+  local revision = 0;
+  if file.exists(constants.revFileName) then
     revision = file.getcontents(constants.revFileName) + 1;
   end
   file.putcontents(constants.revFileName, revision);
-  utils.debug('Revision set to ' .. gossip.currentState.revision);
+  utils.debug('Revision set to ' .. revision);
   return revision;
 end
 
-state.setRevManually = function(revNumber)
+state.setRevFileValue = function(revNumber)
   if revNumber then
-  state.setRev(revNumber);
-  utils.debug('Revision overriden to ' .. revNumber);
+    file.putcontents(constants.revFileName, revNumber);
+    utils.debug('Revision overriden to ' .. revNumber);
   else
     utils.debug('Please provide a revision number.');
   end
@@ -99,10 +100,12 @@ state.start = function()
     return;
   end
   gossip.ip = wifi.sta.getip();
-  if gossip.ip then
+  if not gossip.ip then
     utils.debug('Node not connected to network. Gossip will not start.');
     return;
   end
+
+  gossip.networkState[gossip.ip] = {};
   local localState = gossip.networkState[gossip.ip];
   localState.revision = state.setRev();
   localState.heartbeat = tmr.time();
@@ -113,10 +116,13 @@ state.start = function()
   gossip.inboundSocket:on('receive', network.receiveData);
 
   gossip.started = true;
+
   gossip.timer = tmr.create();
   gossip.timer:register(gossip.config.roundInterval, tmr.ALARM_AUTO,
                         network.sendSyn);
   gossip.timer:start();
+
+  utils.debug('Gossip started.');
 end
 
 state.tickNodeState = function(ip)
@@ -133,27 +139,29 @@ end
 
 network.pushGossip = function(data, ip)
   gossip.networkState[gossip.ip].data = data;
-  network.sendSyn(ip);
+  network.sendSyn(nil, ip);
 end
 
 network.updateNetworkState = function(updateData)
   if gossip.updateCallback then gossip.updateCallback(updateData); end
   for ip, data in pairs(updateData) do
-    if not gossip.config.seedList[ip] then
+    if not utils.contains(gossip.config.seedList, ip) then
       table.insert(gossip.config.seedList, ip);
     end
     gossip.networkState[ip] = data;
   end
 end
 
-network.sendSyn = function(ip)
+-- luacheck: push no unused
+network.sendSyn = function(t, ip)
   local destination = ip or network.pickRandomNode();
   gossip.networkState[gossip.ip].heartbeat = tmr.time();
   if destination then
-    network.sendData(randomNode, gossip.networkState, constants.updateType.SYN);
-    state.tickNodeState(randomNode);
+    network.sendData(destination, gossip.networkState, constants.updateType.SYN);
+    state.tickNodeState(destination);
   end
 end
+-- luacheck: pop
 
 network.pickRandomNode = function()
   if #gossip.config.seedList > 0 then
@@ -177,21 +185,22 @@ end
 
 network.receiveSyn = function(ip, synData)
   utils.debug('Received SYN from ' .. ip);
-  local update, diff = utils.getUpdateDiffDelta(synData);
+  local update = utils.getMinus(synData, gossip.networkState);
+  local diff = utils.getMinus(gossip.networkState, synData);
   network.updateNetworkState(update);
   network.sendAck(ip, diff);
 end
 
 network.receiveAck = function(ip, ackData)
   utils.debug('Received ACK from ' .. ip);
-  local update = utils.getUpdateDiffDelta(ackData);
-  utils.updateNetworkState(update);
+  local update = utils.getMinus(ackData, gossip.networkState);
+  network.updateNetworkState(update);
 end
 
 network.sendAck = function(ip, diff)
-  local diffIps;
-  for k in pairs(diff) do diffIps = diffIps .. k; end
-  utils.log('Sending ACK to ' .. ip .. ' with ' .. diffIps .. ' updates.');
+  local diffIps = '';
+  for k in pairs(diff) do diffIps = diffIps ..' '.. k; end
+  utils.debug('Sending ACK to ' .. ip .. ' with ' .. diffIps .. ' updates.');
   network.sendData(ip, diff, constants.updateType.ACK);
 end
 
@@ -220,13 +229,7 @@ end
 
 -- Constants
 
-constants.nodeState = {
-  TICK = 1,
-  UP = 0,
-  SUSPECT = 2,
-  DOWN = 3,
-  REMOVE = 4
-};
+constants.nodeState = {TICK = 1, UP = 0, SUSPECT = 2, DOWN = 3, REMOVE = 4};
 
 constants.defaultConfig = {
   seedList = {},
@@ -235,16 +238,9 @@ constants.defaultConfig = {
   debug = false
 };
 
-constants.comparisonFields = {
-  'revision',
-  'heartbeat',
-  'state'
-};
+constants.comparisonFields = {'revision', 'heartbeat', 'state'};
 
-constants.updateType = {
-  ACK = 'ACK',
-  SYN = 'SYN'
-}
+constants.updateType = {ACK = 'ACK', SYN = 'SYN'}
 
 constants.revFileName = 'gossip/rev.dat';
 
@@ -255,7 +251,7 @@ gossip = {
   config = constants.defaultConfig,
   setConfig = utils.setConfig,
   start = state.start,
-  setRevManually = state.setRevManually,
+  setRevFileValue = state.setRevFileValue,
   networkState = {},
   getNetworkState = utils.getNetworkState,
   pushGossip = network.pushGossip
@@ -264,15 +260,15 @@ gossip = {
 -- unit tests
 
 -- uncomment this and comment the code below it to run gossip_tests
-  -- return {
-  --   _gossip = gossip,
-  --   _constants = constants,
-  --   _utils = utils,
-  --   _network = network,
-  --   _state = state
-  -- };
+-- return {
+--   _gossip = gossip,
+--   _constants = constants,
+--   _utils = utils,
+--   _network = network,
+--   _state = state
+-- };
 
-if nat and file and tmr and wifi then
+if net and file and tmr and wifi then
   return gossip;
 else
   error('Gossip requires these modules to work: net, file, tmr, wifi');
