8b5606f8bf3fcc0a3b2e42ecd25f7ee8cda8873f
Galion <galion@sdf.org>
2020-08-29 00:03:34 +0200

    Add missing features from Travis CI test build

--- tools/travis/pr-build.sh
+++ tools/travis/pr-build.sh
@@ -6,17 +6,28 @@ echo "Running PR build (all modules, SSL enabled, debug enabled)"
 (
 cd "$TRAVIS_BUILD_DIR"/app/include || exit
 # uncomment disabled modules e.g. '//#define LUA_USE_MODULES_UCG' -> '#define LUA_USE_MODULES_UCG'
-sed -E -i.bak 's@(//.*)(#define *LUA_USE_MODULES_.*)@\2@g' user_modules.h
+sed -i -r 's@(//.*)(#define *LUA_USE_MODULES_.*)@\2@g' user_modules.h
 cat user_modules.h
 
 # enable SSL
-sed -i.bak 's@//#define CLIENT_SSL_ENABLE@#define CLIENT_SSL_ENABLE@' user_config.h
+sed -i 's@//#define CLIENT_SSL_ENABLE@#define CLIENT_SSL_ENABLE@' user_config.h
 
 # enable debug
-sed -E -i.bak 's@// ?#define DEVELOP_VERSION@#define DEVELOP_VERSION@' user_config.h
+sed -i 's@// ?#define DEVELOP_VERSION@#define DEVELOP_VERSION@' user_config.h
 
 # enable FATFS
 sed -i 's@//#define BUILD_FATFS@#define BUILD_FATFS@' user_config.h
+
+# enable new I2C driver
+sed -i 's@#define I2C_MASTER_OLD_VERSION@//#define I2C_MASTER_OLD_VERSION@' user_config.h
+
+# enable pmSleep
+sed -i 's@//#define TIMER_SUSPEND_ENABLE@#define TIMER_SUSPEND_ENABLE@' user_config.h
+sed -i 's@//#define PMSLEEP_ENABLE@#define PMSLEEP_ENABLE@' user_config.h
+
+# enable WiFi SmartConfig
+sed -i 's@//#define WIFI_SMART_ENABLE@#define WIFI_SMART_ENABLE@' user_config.h
+
 cat user_config.h
 
 cd "$TRAVIS_BUILD_DIR"/ld || exit
