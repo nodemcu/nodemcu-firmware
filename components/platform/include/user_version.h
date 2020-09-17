7c07f914836d73e22e862c149def8b3314b77e43
Marcel Stör <marcelstoer@users.noreply.github.com>
2020-09-17 18:17:24 +0200

    Reset user version values
    Undos changes from #3135

--- components/platform/include/user_version.h
+++ components/platform/include/user_version.h
@@ -6,11 +6,12 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU ESP32" " built with Docker provided by frightanic.com\n\tbranch: dev-esp32\n\tcommit: f4887bf134235c05e6c9b2efad370e6d5018f91a\n\tSSL: true\n\tmodules: -\n"
+#define NODE_VERSION	"NodeMCU ESP32"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "created on 2020-05-30 18:01\n"
+#define BUILD_DATE	  "unspecified"
 #endif
 
 #define SDK_VERSION "IDF"
 
 #endif	/* __USER_VERSION_H__ */
+
