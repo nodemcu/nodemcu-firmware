a0c2d70e68a5c85071aeb4e5a3f9228bfcc3e07d
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2020-12-24 00:31:32 +0000

    Add pipeutils example
    These have been handy within the test harness for streaming files and
    LFS images across to the DUTs.

---
+++ lua_examples/pipeutils.lua
@@ -0,0 +1,52 @@
+-- A collection of pipe-based utility functions
+
+-- A convenience wrapper for chunking data arriving in bursts into more sizable
+-- blocks; `o` will be called once per chunk.  The `flush` method can be used to
+-- drain the internal buffer.  `flush` MUST be called at the end of the stream,
+-- **even if the stream is a multiple of the chunk size** due to internal
+-- buffering.  Flushing results in smaller chunk(s) being output, of course.
+local function chunker(o, csize, prio)
+  assert (type(o) == "function" and type(csize) == "number" and 0 <= csize)
+  local p = pipe.create(function(p)
+              -- wait until it looks very likely that read is going to succeed
+              -- and we won't have to unread.  This may hold slightly more than
+              -- a chunk in the underlying pipe object.
+              if 256 * (#p - 2) <= csize then return nil end
+
+              local d = p:read(csize)
+              if #d < csize
+               then p:unread(d) return false
+               else o(d)        return true
+              end
+            end, prio or node.task.LOW_PRIORITY)
+
+  return {
+    flush = function() for d in p:reader(csize) do o(d) end end,
+    write = function(d) p:write(d) end
+  }
+end
+
+-- Stream and decode lines of base64, calling `o(data)` with decoded chunks or
+-- calling `e(badinput, errorstr)` on error; the error callback must ensure
+-- that this conduit is never written to again.
+local function debase64(o, e, prio)
+  assert (type(o) == "function" and type(e) == "function")
+  local p = pipe.create(function(p)
+              local eds = p:read("\n")
+              while #eds > 0 do
+                local ed
+                ed, eds = eds:match("^%s*(%S*)%s*(.*)$")
+                if #ed == 0 then break end
+                local ok, d = pcall(encoder.fromBase64, ed)
+                if ok then o(d) else e(ed, d); p = nil; return false end
+              end
+              p:unread(eds)
+              return false
+            end, prio or node.task.LOW_PRIORITY)
+  return { write = function(d) p:write(d) end }
+end
+
+return {
+  chunker = chunker,
+  debase64 = debase64,
+}
