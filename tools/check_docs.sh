4ef4fb6884c2b4906083b28dbc9f2801adab45b8
Marcel Stör <marcelstoer@users.noreply.github.com>
2021-01-08 12:12:27 +0100

    Harmonize whitepsace

--- tools/check_docs.sh
+++ tools/check_docs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # get all linked module docs for mkdocs.yml
-grep "modules/" mkdocs.yml |sed "s/ *- .*: *'//" | sed "s/'//" | sort> /tmp/doc
+grep "modules/" mkdocs.yml | sed "s/ *- .*: *'//" | sed "s/'//" | sort > /tmp/doc
 
-# get all module and lus_module *.md files
-ls docs/modules/* docs/lua-modules/*|sed "sxdocs/xx" | sort > /tmp/files
+# get all module and lua_module *.md files
+ls docs/modules/* docs/lua-modules/* | sed "sxdocs/xx" | sort > /tmp/files
 
 diff /tmp/doc /tmp/files && echo "all *.md files are reflected in mkdocs.yml"
-
