91b77ca28d5b53a81064eee3aca4bb1c19790718
npt-1707 <npthanh132@gmail.com>
2026-03-11 17:17:04 +0800

    app/lua/ldo.c: disable loading lua bytecode

--- app/lua/ldo.c
+++ app/lua/ldo.c
@@ -513,7 +513,7 @@ static void f_parser (lua_State *L, void *ud) {
   int c = luaZ_lookahead(p->z);
   luaC_checkGC(L);
   set_block_gc(L);  /* stop collector during parsing */
-  tf = ((c == LUA_SIGNATURE[0]) ? luaU_undump : luaY_parser)(L, p->z,
+  tf = (luaY_parser)(L, p->z,
                                                              &p->buff, p->name);
   cl = luaF_newLclosure(L, tf->nups, hvalue(gt(L)));
   cl->l.p = tf;
